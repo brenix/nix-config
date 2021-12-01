05630f3502016f101dbfcf43c9273893fa43b544
Paul Nicholson <brenix@gmail.com>
2021-11-30 21:32:16 -0800

    starship: Move starship config to separate file

--- users/brenix/home.nix
+++ users/brenix/home.nix
@@ -5,6 +5,7 @@
     ./dunst.nix
     ./firefox.nix
     ./git.nix
+    ./starship.nix
     #./polybar.nix
   ];
 
@@ -48,52 +49,6 @@
   # TODO: SSH
   # TODO: Terraform
   # TODO: ZSH
-
-
-  # Starship
-  programs.starship = {
-    enable = true;
-    settings = {
-      add_newline = false;
-      character = {
-        success_symbol = "[▶](bold green)";
-        error_symbol = "[▶](bold red)";
-      };
-      aws = {
-        disabled = false;
-        format = "[$profile(\$region\))]($style) ";
-      };
-      directory = {
-        disabled = false;
-        style = "blue";
-      };
-      hostname = {
-        disabled = false;
-        ssh_only = true;
-        format = "[$hostname]($style) ";
-      };
-      terraform = {
-        disabled = false;
-        format = "";
-      };
-      git_branch = {
-        disabled = false;
-        format = "[\\(](white)[$branch](cyan)[\\)](white) ";
-      };
-      python = {
-        disabled = false;
-        format = "[(\($virtualenv\))]($style) ";
-      };
-
-      cmd_duration.disabled = true;
-      golang.disabled = true;
-      helm.disabled = true;
-      kubernetes.disabled = true;
-      line_break.disabled = true;
-      lua.disabled = true;
-      username.disabled = true;
-    };
-  };
 
   # TMUX
   programs.tmux = {
