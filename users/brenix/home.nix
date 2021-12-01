a0c0db56d239831828fee5fc3a30ed601c07ddf0
Paul Nicholson <brenix@gmail.com>
2021-12-01 15:29:43 -0800

    tmux: Move tmux into separate file

--- users/brenix/home.nix
+++ users/brenix/home.nix
@@ -6,7 +6,7 @@
     ./firefox.nix
     ./git.nix
     ./starship.nix
-    #./polybar.nix
+    ./tmux.nix
   ];
 
   # Enable home-manager
@@ -67,34 +67,6 @@
   # TODO: SSH
   # TODO: Terraform
   # TODO: ZSH
-
-  # TMUX
-  programs.tmux = {
-    enable = true;
-    prefix = "C-x";
-    terminal = "tmux-256color";
-    escapeTime = 0;
-    baseIndex = 1;
-    extraConfig = ''
-      set-option -g mouse on
-      bind | split-window -h -c "#{pane_current_path}"
-      bind - split-window -v -c "#{pane_current_path}"
-      bind n new-window -c "#{pane_current_path}"
-      bind tab next-window
-      bind t command-prompt "rename-window %%"
-      bind , select-layout even-vertical
-      bind . select-layout even-horizontal
-      bind v setw synchronize-panes\; display "Sync panes is now #{?pane_synchronized,on,off}!"
-      bind h select-pane -L
-      bind l select-pane -R
-      bind k select-pane -U
-      bind j select-pane -D
-      bind up resize-pane -U 5
-      bind down resize-pane -D 5
-      bind left resize-pane -L 5
-      bind right resize-pane -R 5
-    '';
-  };
 
   programs.rofi = {
     enable = true;
