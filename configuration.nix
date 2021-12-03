5919bd08e5b6c68a91542ab956c6fcc1a0c7caad
Paul Nicholson <brenix@gmail.com>
2021-12-02 20:32:44 -0800

    systemd: speed up boot

--- configuration.nix
+++ configuration.nix
@@ -126,6 +126,11 @@
     RuntimeDirectorySize=50%
   '';
 
+  # Speed up boot
+  # https://discourse.nixos.org/t/boot-faster-by-disabling-udev-settle-and-nm-wait-online/6339
+  systemd.services.systemd-udev-settle.enable = false;
+  systemd.services.NetworkManager-wait-online.enable = false;
+
   # -- FONTS
 
   # Common fonts to install
