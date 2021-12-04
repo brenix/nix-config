9ee08177a06b01988d30b4540a78224b57ae7ec6
Paul Nicholson <brenix@gmail.com>
2021-12-04 15:51:10 -0800

    openbox: Add nord theme files

--- home/openbox/default.nix
+++ home/openbox/default.nix
@@ -1,6 +1,13 @@
-{ pkgs, ... }: {
+{ pkgs, ...}: {
 
-  xdg.configFile."openbox".source = "./menu.xml";
-  xdg.configFile."openbox".source = "./rc.xml";
+  xdg.configFile = {
+    "openbox/rc.xml".source = ./rc.xml;
+    "openbox/menu.xml".source = ./menu.xml;
+  };
+
+  home.file.".local/share/themes/Nord" = {
+    source = themes/Nord;
+    recursive = true;
+  };
 
 }
