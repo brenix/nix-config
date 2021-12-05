9aa4a83bb135500c65258f49a574e0c27815bb39
Paul Nicholson <brenix@gmail.com>
2021-12-05 14:13:30 -0800

    openbox: restore feh background on start

--- home/openbox/default.nix
+++ home/openbox/default.nix
@@ -3,6 +3,7 @@
   xdg.configFile = {
     "openbox/rc.xml".source = ./rc.xml;
     "openbox/menu.xml".source = ./menu.xml;
+    "openbox/autostart".text = "$HOME/.fehbg\n";
   };
 
   home.file.".local/share/themes/Nord" = {
