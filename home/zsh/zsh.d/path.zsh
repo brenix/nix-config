8818c0f80243ec6eb6303b6939b9c2eae3c716e8
Paul Nicholson <brenix@gmail.com>
2021-12-07 10:18:05 -0800

    zsh: add nix profile for completion paths

--- home/zsh/zsh.d/path.zsh
+++ home/zsh/zsh.d/path.zsh
@@ -14,6 +14,11 @@ path=(
   $path[@]
 )
 
+fpath=(
+  $HOME/.nix-profile/share/zsh/site-functions
+  $fpath[@]
+)
+
 # override with gnu core utils (darwin)
 if [[ "$OSTYPE" == "darwin"* ]]; then
   export PATH=/usr/local/opt/coreutils/libexec/gnubin:/usr/local/opt/make/libexec/gnubin:/usr/local/opt/findutils/libexec/gnubin:/usr/local/opt/gnu-sed/libexec/gnubin:/usr/local/opt/grep/libexec/gnubin:${PATH}
