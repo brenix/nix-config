f92475e417165b03136f7a72059316c10a8b3ae0
Paul Nicholson <brenix@gmail.com>
2021-12-16 13:29:13 -0800

    update nvim config

--- home/neovim/nvim/lua/config/null-ls.lua
+++ home/neovim/nvim/lua/config/null-ls.lua
@@ -3,14 +3,17 @@ local _, null_ls = pcall(require, "null-ls")
 local b = null_ls.builtins
 
 local sources = {
+  b.formatting.gofmt,
+  b.formatting.goimports,
+  b.formatting.nixfmt,
   b.formatting.prettier,
+  b.formatting.shellharden,
   b.formatting.shfmt.with({
     extra_args = { "-i", "2", "-ci" }
   }),
-  b.formatting.goimports,
-  b.formatting.gofmt,
   b.formatting.stylua,
   b.formatting.terraform_fmt,
+  b.formatting.trim_whitespace,
 }
 
 local null_ls = require("null-ls")
