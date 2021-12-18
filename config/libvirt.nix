5e7e8ca683db4623aa21f036d1ad78dcf0dd9fc2
Paul Nicholson <brenix@gmail.com>
2021-12-18 10:33:30 -0800

    WIP libvirt

--- config/libvirt.nix
+++ config/libvirt.nix
@@ -1,5 +1,21 @@
-{ ... }: {
+{ config, ... }: {
 
-  virtualisation.libvirtd = { enable = true; };
+  imports = [ ../modules/settings.nix ];
 
+  virtualisation.libvirtd = {
+    enable = true;
+    enableKVM = true;
+    qemu = {
+      ovmf.enable = true;
+      runAsRoot = false;
+      verbatimConfig = ''
+        security_driver = "none"
+        security_default_confined = 0
+        user = ${config.settings.username}
+        seccomp_sandbox = 0
+      '';
+    };
+  };
+
+  # TODO: libvirt-hooks
 }
--- hosts/neo.nix
+++ hosts/neo.nix
@@ -1,61 +1,82 @@
-{ config, ... }:
+{ config, lib, ... }:
 let
   devices = [
-    { device = "10de:2206"; slot = "0b:00.0"; } # GPU
-    { device = "10de:1aef"; slot = "0b:00.1"; } # GPU HD AUDIO
-    { device = "8086:1539"; slot = "06:00.0"; } # I211 Gigabit Ethernet
+    # GPU
+    {
+      device = "10de:2206";
+      slot = "0b:00.0";
+    }
+    # GPU AUDIO
+    {
+      device = "10de:1aef";
+      slot = "0b:00.1";
+    }
+    # I211 Gigabit Ethernet
+    {
+      device = "8086:1539";
+      slot = "06:00.0";
+    }
   ];
-in
-{
+in {
   boot = {
     kernelParams = [
       "amd_iommu=on"
       "default_hugepagesz=1G"
       "hugepagesz=1G"
-      "transparent_hugepage=never"
-      "nohz_full=8-15,24-31"
-      "rcu_nocbs=8-15,24-31"
       "iommu=pt"
-      "rd.driver.pre=vfio-pci"
-      "systemd.unified_cgroup_hierarchy=1"
-      "usbcore.autosuspend=-1"
-      "tsc=reliable"
       "mitigations=off"
       "module_blacklist=xhci_pci"
-      "quiet"
-      "video=efifb:off"
+      "nohz_full=8-15,24-31"
+      "rcu_nocbs=8-15,24-31"
+      "rd.driver.pre=vfio-pci"
+      "systemd.unified_cgroup_hierarchy=1"
+      "transparent_hugepage=never"
+      "tsc=reliable"
+      "usbcore.autosuspend=-1"
+      "vfio-pci.ids=${lib.concatMapStringsSep "," (d: d.device) devices}"
     ];
-    blacklistedKernelModules = [ "snd_hda_intel" "nvidia" "nouveau" ];
-    kernelModules = [ "kvm-amd" "vfio_virqfd" "vfio_pci" "vfio_iommu_type1" "vfio" ];
+
+    blacklistedKernelModules = [ "nouveau" ];
+
+    kernelModules =
+      [ "kvm-amd" "vfio_virqfd" "vfio_pci" "vfio_iommu_type1" "vfio" ];
 
     extraModprobeConfig = ''
-      options vfio-pci ids=${lib.concatMapStringsSep "," (d: d.device) devices}
       options kvm ignore_msrs=1
+      options kvm avic=1
     '';
   };
-}
 
   # Hostname
   networking.hostName = "neo";
 
-# Enable DHCP
-systemd.network.networks.enp7s0 = {
-matchConfig = { Name = "enp7s0"; };
-DHCP = "yes";
-};
+  # Enable DHCP
+  systemd.network.networks.enp7s0 = {
+    matchConfig = { Name = "enp7s0"; };
+    DHCP = "yes";
+  };
 
-# DPI settings
-services.xserver.dpi = 109;
+  # DPI settings
+  services.xserver.dpi = 109;
 
-# Fix scaling in GTK apps
-environment.variables.GDK_SCALE = "1";
-environment.variables.GDK_DPI_SCALE = "1";
+  # Fix scaling in GTK apps
+  environment.variables.GDK_SCALE = "1";
+  environment.variables.GDK_DPI_SCALE = "1";
 
-# Configure host-specific settings
-settings = { dpi = 109; };
+  # Configure host-specific settings
+  settings = {
+    dpi = 109;
+    fonts = {
+      browser.font = "Verdana";
+      browser.size = 16;
+      launcher.font = "Verdana";
+      launcher.size = 10;
+      terminal.font = "JetBrains Mono Nerd Font";
+      terminal.size = 10.5;
+    };
+  };
 
-# Pass settings to home-manager
-home-manager.users.${config.settings.username}.settings = config.settings;
-
+  # Pass settings to home-manager
+  home-manager.users.${config.settings.username}.settings = config.settings;
 
 }
