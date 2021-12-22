4b45199ee0f03fd0a7ed1c9f5bd0edfa73f6edaf
Paul Nicholson <brenix@gmail.com>
2021-12-21 20:38:51 -0800

    Add hack to fix polybar annoyance

--- home/openbox/default.nix
+++ home/openbox/default.nix
@@ -4,11 +4,13 @@
 
   xsession = { windowManager = { command = "openbox-session"; }; };
 
+  # HACK: Figure out solution for the need to restart polybar
   xdg.configFile = {
     "openbox/rc.xml".source = ./rc.xml;
     "openbox/menu.xml".source = ./menu.xml;
     "openbox/autostart".text = ''
       ${pkgs.feh}/bin/feh --no-fehbg --bg-scale $HOME/.background-image
+      ${pkgs.systemd}/bin/systemctl --user restart polybar.service
     '';
   };
 
