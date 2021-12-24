f1f5731dfcbe2847a2fd840c503564d31d4ac93a
Paul Nicholson <brenix@gmail.com>
2021-12-23 22:16:28 -0800

    Fix battery icon and cleanup

--- home/polybar/default.nix
+++ home/polybar/default.nix
@@ -9,7 +9,10 @@
 
     settings = let colors = config.colorscheme.colors;
     in {
-      "settings" = { screenchange-reload = true; };
+      "settings" = {
+        screenchange-reload = true;
+        format-padding = 1;
+      };
 
       "colors" = {
         bg = "#${colors.base00}";
@@ -23,8 +26,6 @@
         purple = "#${colors.base0F}";
         red = "#${colors.base08}";
         yellow = "#${colors.base0A}";
-        trans = "#00000000";
-        semi-trans-black = "#aa000000";
       };
 
       "bar/main" = {
@@ -33,7 +34,6 @@
         dpi = config.settings.dpi;
         enable-ipc = true;
         width = "100%";
-        spacing = 0;
         foreground = "\${colors.fg}";
         background = "\${colors.bg}";
         border-bottom-color = "\${colors.bg}";
@@ -46,7 +46,7 @@
         module-margin-left = 1;
         module-margin-right = 1;
         modules-left = "workspaces";
-        modules-center = "previous playpause next spotify";
+        modules-center = "";
         modules-right = "battery cpu temperature memory volume date time";
         offset-x = 0;
         offset-y = 0;
@@ -58,8 +58,18 @@
         type = "internal/battery";
         battery = "BAT1";
         adapter = "ACAD";
-        format-charging-prefix = " ";
-        format-discharging-prefix = " ";
+        format-discharging = " <label-discharging>";
+        format-charging = " <label-charging>";
+        format-full = " <label-full>";
+        label-charging = "%percentage%%";
+        label-discharging = "%percentage%%";
+        label-full = "%percentage%%";
+        # ramp-capacity-0 = "";
+        # ramp-capacity-1 = "";
+        # ramp-capacity-2 = "";
+        # ramp-capacity-3 = "";
+        # ramp-capacity-4 = "";
+        poll-interval = 10;
       };
 
       "module/workspaces" = {
@@ -67,12 +77,12 @@
         enable-click = true;
         enable-scroll = true;
         format = "<label-state>";
-        label-focused = "%icon%";
-        label-focused-padding = 1;
-        # label-focused-background = "\${colors.blue}";
-        label-focused-foreground = "\${colors.blue}";
+        format-padding = 0;
         label-empty = "%icon%";
         label-empty-padding = 1;
+        label-focused = "%icon%";
+        label-focused-foreground = "\${colors.blue}";
+        label-focused-padding = 1;
         label-occupied = "%icon%";
         label-occupied-padding = 1;
         label-urgent = "%icon%";
@@ -82,58 +92,12 @@
         ws-icon-2 = "3;";
         ws-icon-3 = "4;";
         ws-icon-default = "";
-        #ws-icon-default = "";
-      };
-
-      "module/mpd" = {
-        type = "internal/mpd";
-        host = "127.0.0.1";
-        port = 6600;
-        interval = 5;
-        format-online =
-          "<icon-prev> <toggle> <icon-next> <icon-stop> <label-song>";
-        label-song = "%artist% - %title%";
-        format-stopped = "";
-        click-left = "mpc -q toggle";
-        icon-play = "";
-        icon-pause = "";
-        icon-stop = "";
-        icon-prev = "";
-        icon-next = "";
-      };
-
-      "module/previous" = {
-        type = "custom/ipc";
-        hook-0 = "echo ''";
-        hook-1 = "echo ''";
-        click-left = "spotifyctl -q previous";
-      };
-
-      "module/next" = {
-        type = "custom/ipc";
-        hook-0 = "echo ''";
-        hook-1 = "echo ''";
-        click-left = "spotifyctl -q next";
-      };
-
-      "module/playpause" = {
-        type = "custom/ipc";
-        hook-0 = "echo ''";
-        hook-1 = "echo ''";
-        hook-2 = "echo ''";
-        click-left = "spotifyctl -q playpause";
-      };
-
-      "module/spotify" = {
-        type = "custom/ipc";
-        hook-0 = "echo ''";
-        hook-1 = "spotifyctl -q status --format '%artist%: %title%'";
       };
 
       "module/temperature" = {
         type = "internal/temperature";
         interval = 3;
-        # hwmon-path = "/sys/devices/pci0000:00/0000:00:18.3/hwmon/hwmon3/temp1_input";
+        hwmon-path = "\${env:HWMON_PATH}";
         thermal-zone = 0;
         base-temperature = 20;
         warn-temperature = 60;
@@ -142,7 +106,6 @@
         label-warn-foreground = "\${colors.red}";
         format = "<label>";
         format-prefix = " ";
-        format-prefix-foreground = "\${colors.fg}";
         format-warn = "<label-warn>";
       };
 
@@ -151,7 +114,6 @@
         interval = 3;
         label = "%percentage:2%%";
         format-prefix = " ";
-        format-prefix-foreground = "\${colors.fg}";
       };
 
       "module/memory" = {
@@ -159,20 +121,18 @@
         interval = 30;
         label = "%gb_used%";
         format-prefix = " ";
-        format-prefix-foreground = "\${colors.fg}";
       };
 
       "module/volume" = {
         type = "internal/pulseaudio";
-        # sink = "alsa_output.usb-C-Media_Electronics_Inc._USB_Audio_Device-00.analog-stereo";
         use-ui-max = false;
         interval = 2;
+        bar-volume-empty-foreground = "\${colors.fg-alt}";
         format-volume = "<ramp-volume> <label-volume>";
-        label-volume = "%{A3:pavucontrol & disown:}%percentage%%%{A}";
-        label-volume-foreground = "\${root.foreground}";
         label-muted = " %{F#b77a76}MUTED";
         label-muted-foreground = "\${colors.fg-alt}";
-        bar-volume-empty-foreground = "\${colors.fg-alt}";
+        label-volume =
+          "%{A3:${pkgs.pavucontrol}/bin/pavucontrol & disown:}%percentage%%%{A}";
         ramp-volume-0 = "";
         ramp-volume-1 = "";
         ramp-volume-2 = "";
@@ -184,7 +144,6 @@
         time = "%r";
         label = "%time%";
         format-prefix = " ";
-        format-prefix-foreground = "\${colors.fg}";
       };
 
       "module/date" = {
