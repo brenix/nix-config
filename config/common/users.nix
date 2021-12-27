ddc90f75c3abd26220af28bf4ac81332b55e5345
Paul Nicholson <brenix@gmail.com>
2021-12-26 19:59:35 -0800

    Update user groups

--- config/common/users.nix
+++ config/common/users.nix
@@ -9,7 +9,8 @@
     createHome = true;
     home = "/home/${config.settings.username}";
     description = config.settings.name;
-    extraGroups = [ "wheel" "libvirtd" "kvm" "input" ];
+    extraGroups =
+      [ "input" "kvm" "libvirtd" "polkituser" "qemu-libvirtd" "wheel" ];
     shell = pkgs.zsh;
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG++dlRrheRZgVLtzadOWFJgHgEL27t70oUZyLwL1o0F 20170524-brenix@gmail.com"
