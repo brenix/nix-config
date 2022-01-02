10d051b46881d3cfa2b8dd32964b93f4f74db057
Paul Nicholson <brenix@gmail.com>
2022-01-02 10:59:56 -0800

    Enable doas alongside sudo

--- config/common/security.nix
+++ config/common/security.nix
@@ -1,9 +1,24 @@
-{ ... }: {
+{ config, ... }: {
+
+  imports = [ ../../modules/settings.nix ];
 
   # Prevent replacing the running kernel image
   security.protectKernelImage = true;
 
   # Dont prompt for sudo password
   security.sudo.wheelNeedsPassword = false;
+
+  # Disable sudo
+  # security.sudo.enable = false;
+
+  # Enable doas
+  security.doas.enable = true;
+
+  # Configure doas
+  security.doas.extraRules = [{
+    users = [ config.settings.username ];
+    keepEnv = true;
+    noPass = true;
+  }];
 
 }
