8e23e927548768f9244c7be5b9fba4e3020f0ea6
Paul Nicholson <brenix@gmail.com>
2022-01-14 10:09:05 -0800

    Use xanmod kernel for a bit

--- config/common/bootloader.nix
+++ config/common/bootloader.nix
@@ -1,7 +1,7 @@
 { pkgs, lib, ... }: {
 
   boot = {
-    kernelPackages = lib.mkDefault pkgs.linuxPackages_latest;
+    kernelPackages = lib.mkDefault pkgs.linuxPackages_xanmod;
 
     cleanTmpDir = true;
 
