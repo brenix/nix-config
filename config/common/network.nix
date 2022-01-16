f5db4c32cd312fab6a37a2d0af066e1bcb11f93f
Paul Nicholson <brenix@gmail.com>
2022-01-15 21:16:08 -0800

    Remove systemd resolved domains

--- config/common/network.nix
+++ config/common/network.nix
@@ -4,7 +4,9 @@
 
   # Enable systemd-resolved
   services.resolved.enable = lib.mkDefault true;
-  services.resolved.domains = [ "localdomain" ];
+
+  # NOTE: Disabled since it doesn't seem to help and borked k8s a bit
+  #services.resolved.domains = [ "localdomain" ];
 
   # Enable systemd-networkd
   networking.dhcpcd.enable = false;
