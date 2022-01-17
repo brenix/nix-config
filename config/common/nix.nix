213672e116437359fb44c4f5d28c7293f7ffe930
Paul Nicholson <brenix@gmail.com>
2022-01-16 18:00:06 -0800

    Add community cache

--- config/common/nix.nix
+++ config/common/nix.nix
@@ -2,7 +2,16 @@
   nixpkgs.config.allowUnfree = true;
 
   nix = {
-    package = pkgs.nixFlakes;
+    package = pkgs.nixUnstable;
+
+    generateNixPathFromInputs = true;
+    generateRegistryFromInputs = true;
+    linkInputs = true;
+
+    binaryCaches = [ "https://nix-community.cachix.org" ];
+    binaryCachePublicKeys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
 
     autoOptimiseStore = true;
 
@@ -11,6 +20,7 @@
 
     extraOptions = ''
       experimental-features = nix-command flakes
+      builders-use-substitutes = true
     '';
 
     gc = {
