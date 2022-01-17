66f7798e5be3f9e469c9cd799f069d264a2e4268
Paul Nicholson <brenix@gmail.com>
2022-01-16 19:30:05 -0800

    Additional sysctl tunings

--- config/common/sysctl.nix
+++ config/common/sysctl.nix
@@ -4,7 +4,9 @@
     kernelModules = [ "tcp_bbr" ];
     kernel = {
       sysctl = {
+        "net.core.default_qdisc" = "cake";
         "net.ipv4.tcp_congestion_control" = "bbr";
+        "net.ipv4.tcp_fastopen" = 3;
         "net.ipv4.tcp_slow_start_after_idle" = 0;
         "vm.dirty_background_ratio" = 20;
         "vm.dirty_ratio" = 50;
