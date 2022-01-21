47ed33f53759d13bc5e9fb804673e476b778ad08
Paul Nicholson <brenix@gmail.com>
2022-01-21 15:00:54 -0800

    Disable v4l-utils overlay for now

--- overlays/default.nix
+++ overlays/default.nix
@@ -6,14 +6,14 @@ final: prev: {
   calicoctl = final.callPackage ../pkgs/calicoctl.nix { };
 
   # Upgrade v4l-utils until merged upstream
-  v4l-utils = prev.v4l-utils.overrideAttrs (oldAttrs: rec {
-    version = "1.22.1";
-    pname = oldAttrs.pname;
-    src = prev.fetchurl {
-      url =
-        "https://linuxtv.org/downloads/${pname}/${pname}-${version}.tar.bz2";
-      sha256 = "sha256-Zcb76DCkTKEFxEOwJxgsGyyQU6kdHnKthJ36s4i5TjE=";
-    };
-  });
+  # v4l-utils = prev.v4l-utils.overrideAttrs (oldAttrs: rec {
+  # version = "1.22.1";
+  # pname = oldAttrs.pname;
+  # src = prev.fetchurl {
+  # url =
+  # "https://linuxtv.org/downloads/${pname}/${pname}-${version}.tar.bz2";
+  # sha256 = "sha256-Zcb76DCkTKEFxEOwJxgsGyyQU6kdHnKthJ36s4i5TjE=";
+  # };
+  # });
 
 }
