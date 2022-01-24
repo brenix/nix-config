21371f3a7209c93b2358576ebb6741f6dda09734
Paul Nicholson <brenix@gmail.com>
2022-01-23 18:33:37 -0800

    Cleanup unused overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -5,17 +5,6 @@ final: prev: {
   # Install calicoctl
   calicoctl = final.callPackage ../pkgs/calicoctl.nix { };
 
-  # Upgrade v4l-utils until merged upstream
-  # v4l-utils = prev.v4l-utils.overrideAttrs (oldAttrs: rec {
-  # version = "1.22.1";
-  # pname = oldAttrs.pname;
-  # src = prev.fetchurl {
-  # url =
-  # "https://linuxtv.org/downloads/${pname}/${pname}-${version}.tar.bz2";
-  # sha256 = "sha256-Zcb76DCkTKEFxEOwJxgsGyyQU6kdHnKthJ36s4i5TjE=";
-  # };
-  # });
-
   # Use more recent version of nvim-treesitter plugin
   nvim-treesitter = prev.nvim-treesitter.overrideAttrs (old: {
     src = prev.fetchFromGitHub {
