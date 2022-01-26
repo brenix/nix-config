31b17b5de0ae12d9cb58c3d9f94cfad5d1dbf5fd
Paul Nicholson <brenix@gmail.com>
2022-01-25 20:50:43 -0800

    Remove treesitter overlay since upstream fixed

--- overlays/default.nix
+++ overlays/default.nix
@@ -5,12 +5,4 @@ final: prev: {
   # Install calicoctl
   calicoctl = final.callPackage ../pkgs/calicoctl.nix { };
 
-  # Use more recent version of nvim-treesitter plugin
-  nvim-treesitter = prev.nvim-treesitter.overrideAttrs (old: {
-    src = prev.fetchFromGitHub {
-      rev = "bb33aea03cd65e62e0b5cdd0b1077c09b88dce1b";
-      sha256 = "UcuZZNJmWSVfeLAUJ3q+NObDS/rGkXbBCdXcdMBBYXg=";
-    };
-  });
-
 }
