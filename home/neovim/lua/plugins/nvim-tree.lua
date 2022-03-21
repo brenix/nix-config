4896df9d2baa1aaebfe758ba4720625fea59d658
Paul Nicholson <brenix@gmail.com>
2022-03-21 10:24:10 -0700

    nvim-tree tuning

--- home/neovim/lua/plugins/nvim-tree.lua
+++ home/neovim/lua/plugins/nvim-tree.lua
@@ -9,21 +9,24 @@ vim.g.nvim_tree_respect_buf_cwd = 1
 vim.g.nvim_tree_root_folder_modifier = table.concat({ ":t:gs?$?/..", string.rep(" ", 1000), "?:gs?^??" })
 
 require("nvim-tree").setup({
-	auto_close = false,
+	auto_close = true,
 	open_on_tab = false,
-	open_on_setup = true,
+	open_on_setup = false,
 	update_cwd = false,
 	update_focused_file = {
 		enable = true,
 		update_cwd = true,
+	},
+	actions = {
+		open_file = {
+			resize_window = true,
+		},
 	},
 	filters = {
 		dotfiles = false,
 		custom = { ".git", "node_modules", ".cache" },
 	},
 	view = {
-		auto_resize = true,
 		width = 35,
-		preserve_window_proportions = true,
 	},
 })
