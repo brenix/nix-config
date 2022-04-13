26ef53d12457f89908616f5aac819154d9d2ab87
Paul Nicholson <brenix@gmail.com>
2022-04-13 09:28:53 -0700

    Lower vfs_cache_pressure

--- config/common/sysctl.nix
+++ config/common/sysctl.nix
@@ -11,7 +11,7 @@ _: {
         "vm.dirty_background_ratio" = 20;
         "vm.dirty_ratio" = 50;
         "vm.swappiness" = 0;
-        "vm.vfs_cache_pressure" = 50;
+        "vm.vfs_cache_pressure" = 10;
       };
     };
   };
