12ab5aa7cce9457870cd5cdcd9611bcc422652ca
Paul Nicholson <brenix@gmail.com>
2022-04-23 19:50:40 -0700

    move to unstable-small

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     flake-utils.url = "github:gytis-ivaskevicius/flake-utils-plus";
     home-manager = {
