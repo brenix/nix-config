b809acdbea280f4699e230a9e555bd0cf161df5e
Paul Nicholson <brenix@gmail.com>
2022-05-16 09:27:37 -0700

    Simplify authy overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -2,5 +2,13 @@
 
 final: prev: {
   calicoctl = prev.callPackage ./calicoctl { };
-  authy = prev.callPackage ./authy { };
+
+  # Add `--no-sandbox` flag to authy due to issues identifying GPU
+  authy = prev.authy.overrideAttrs (oldAttrs: rec {
+    postFixup = ''
+      makeWrapper ${prev.electron_9}/bin/electron $out/bin/authy \
+        --add-flags $out/resources/app.asar \
+        --add-flags "--no-sandbox"
+    '';
+  });
 }
