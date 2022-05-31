a2b2b21c0b7448028bea47433e7aa2706cfb994a
Paul Nicholson <brenix@gmail.com>
2022-05-31 08:22:09 -0700

    Fix nvim-tree deprecations

--- home/neovim/lua/plugins/nvim-tree.lua
+++ home/neovim/lua/plugins/nvim-tree.lua
@@ -1,17 +1,18 @@
 -- Options
 vim.g.nvim_tree_side = "left"
 vim.g.nvim_tree_width = 25
-vim.g.nvim_tree_git_hl = 1
-vim.g.nvim_tree_root_folder_modifier = ":~"
 vim.g.nvim_tree_allow_resize = 1
-vim.g.nvim_tree_special_files = { "" }
-vim.g.nvim_tree_respect_buf_cwd = 1
-vim.g.nvim_tree_root_folder_modifier = table.concat({ ":t:gs?$?/..", string.rep(" ", 1000), "?:gs?^??" })
 
 require("nvim-tree").setup({
 	open_on_tab = false,
 	open_on_setup = false,
 	update_cwd = false,
+	respect_buf_cwd = true,
+	renderer = {
+    root_folder_modifier = ":~",
+    highlight_git = true,
+    special_files = { "" },
+	},
 	update_focused_file = {
 		enable = true,
 		update_cwd = true,
