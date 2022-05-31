6784070e343eb98c15912c0c4dda6197d38fa835
Paul Nicholson <brenix@gmail.com>
2022-05-31 11:21:37 -0700

    More prompt changes

--- home/starship/default.nix
+++ home/starship/default.nix
@@ -5,38 +5,56 @@ _: {
     settings = {
       add_newline = false;
       format = ''
-        $hostname$shlvl$directory$git_branch$git_commit$git_state$git_status$aws$python$jobs$status$character
-      '';
-      right_format = ''
-        $kubernetes
+        $username$hostname$shlvl$directory$git_branch$git_commit$git_state$git_status$aws$python$kubernetes$fill
+        $jobs$status$character
       '';
       aws = {
-        format = "[$profile \\($region\\)]($style) ";
+        format = "[$profile \\($region\\) ]($style)";
+        style = "bg:#191c26 fg:bold yellow";
+      };
+      fill = {
+        symbol = " ";
+        style = "bg:#191c26";
       };
       character = {
         success_symbol = "[▶](bold green)";
         error_symbol = "[▶](bold red)";
       };
       directory = {
-        style = "blue";
+        format = "[$path ]($style)";
+        style = "bg:#191c26 fg:blue";
       };
       git_branch = {
-        format = "[$branch](cyan) ";
+        format = "[$branch ]($style)";
+        style = "bg:#191c26 fg:cyan";
+      };
+      git_status = {
+        format = "([\\[$all_status$ahead_behind\\] ]($style))";
+        style = "bg:#191c26 fg:bold red";
       };
       hostname = {
         ssh_only = true;
-        format = "[$hostname]($style) ";
+        format = "[$hostname ]($style)";
+        style = "bg:#191c26 fg:bright-black";
       };
       kubernetes = {
         disabled = false;
-        format = "[$context](yellow)[:](bright-black)[$namespace](white)";
+        format = "[\\(](bg:#191c26 fg:white)[$context](bg:#191c26 fg:yellow)[:](bg:#191c26 fg:bright-black)[$namespace](bg:#191c26 fg:white)[\\)](bg:#191c26 fg:white)";
         context_aliases = {
           ".*vdp.*" = "vdp";
           "(?P<var_cell>[\\\\w-]+)-aws-\\\\w+-(?P<var_cluster>[\\\\w-]+)-.*" = "$var_cell-$var_cluster";
         };
       };
       python = {
-        format = "[(($virtualenv))]($style) ";
+        format = "[(($virtualenv)) ]($style)";
+        style = "bg:#191c26 fg:purple bold";
+      };
+      username = {
+        disabled = true;
+        show_always = false;
+        format = "[$user]($style)";
+        style_user = "bg:#191c26 fg:bright-black";
+        style_root = "bg:#191c26 fg:bold red";
       };
     };
   };
