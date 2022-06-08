b91874d1840df85bc65e5d15e4c71cbef8586240
Paul Nicholson <brenix@gmail.com>
2022-06-08 16:46:18 -0700

    Add default config for myrepos

--- home/default.nix
+++ home/default.nix
@@ -262,6 +262,18 @@
         ];
       };
 
+      # myrepos
+      home.file.".mrconfig".text = ''
+        [DEFAULT]
+        jobs = 5
+        git_update = git pull --prune --tags "$@"
+        git_gc = git gc --aggressive "$@"
+        git_tags = git tag -l
+        branch = printf "\e[1;33m%-6s\e[m\n" $(git rev-parse --abbrev-ref HEAD)
+        default = git checkout $(git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@')
+        reset = git reset --hard HEAD && git checkout $(git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@') && git clean -d -f
+      '';
+
       # ssh
       programs.ssh = {
         enable = true;
