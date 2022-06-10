ffaf7a68f44e11d242e4e21c7703d792fb1c47d6
Paul Nicholson <brenix@gmail.com>
2022-06-10 08:09:16 -0700

    Update flake

--- home/default.nix
+++ home/default.nix
@@ -269,6 +269,7 @@
         git_update = git pull --prune --tags "$@"
         git_gc = git gc --aggressive "$@"
         git_tags = git tag -l
+        git_branches = git branch
         branch = printf "\e[1;33m%-6s\e[m\n" $(git rev-parse --abbrev-ref HEAD)
         default = git checkout $(git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@')
         reset = git reset --hard HEAD && git checkout $(git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@') && git clean -d -f
