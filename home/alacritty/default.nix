0508f034293d127fd4063a9146a61c37a462d853
Paul Nicholson <brenix@gmail.com>
2022-06-12 15:21:48 -0700

    Update font size

--- home/alacritty/default.nix
+++ home/alacritty/default.nix
@@ -17,8 +17,8 @@
         family = "JetBrainsMono Nerd Font";
         style = "Italic";
       };
-      font.size = lib.mkDefault 10.5;
-      font.offset.y = -2;
+      font.size = lib.mkDefault 9;
+      font.offset.y = -3;
       font.use_thin_strokes = false;
       draw_bold_text_with_bright_colors = false;
       mouse_bindings = [{
