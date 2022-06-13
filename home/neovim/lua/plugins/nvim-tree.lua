d89cdd5b07e625aa212bce1cc3958a46fa257820
Paul Nicholson <brenix@gmail.com>
2022-06-13 09:29:26 -0700

    Update nvim-tree settings

--- home/neovim/lua/plugins/nvim-tree.lua
+++ home/neovim/lua/plugins/nvim-tree.lua
@@ -1,32 +1,29 @@
--- Options
-vim.g.nvim_tree_side = "left"
-vim.g.nvim_tree_width = 25
-vim.g.nvim_tree_allow_resize = 1
-
 require("nvim-tree").setup({
-  open_on_tab = false,
-  open_on_setup = false,
-  update_cwd = false,
-  respect_buf_cwd = true,
-  renderer = {
-    root_folder_modifier = ":~",
-    highlight_git = true,
-    special_files = { "" },
-  },
-  update_focused_file = {
-    enable = true,
-    update_cwd = true,
-  },
-  actions = {
-    open_file = {
-      resize_window = true,
-    },
-  },
-  filters = {
-    dotfiles = false,
-    custom = { ".git", "node_modules", ".cache" },
-  },
-  view = {
-    width = 35,
-  },
+	auto_reload_on_write = false,
+	open_on_tab = false,
+	open_on_setup = false,
+	update_cwd = false,
+	respect_buf_cwd = true,
+	reload_on_bufenter = true,
+	renderer = {
+		root_folder_modifier = ":~",
+		highlight_git = true,
+		special_files = { "" },
+	},
+	update_focused_file = {
+		enable = true,
+		update_cwd = true,
+	},
+	actions = {
+		open_file = {
+			resize_window = true,
+		},
+	},
+	filters = {
+		dotfiles = false,
+		custom = { ".git", "node_modules", ".cache" },
+	},
+	view = {
+		width = 40,
+	},
 })
