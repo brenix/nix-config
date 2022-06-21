d2813f79c23161cf63a0911310b739e776f3ced5
Paul Nicholson <brenix@gmail.com>
2022-06-21 14:07:30 -0700

    Use bin packages for packer and terraform

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -3,6 +3,16 @@
 final: prev: {
   calicoctl = prev.callPackage ./calicoctl { };
   dash-font = prev.callPackage ./dash-font { };
+  packer = prev.callPackage (import ./hashicorp/generic.nix) {
+    name = "packer";
+    version = "1.8.0";
+    sha256 = "sha256-lMXWU4b6jxfjbTSW+73xjTv6sBMGqoAAwUZWtjBwt8I=";
+  };
+  terraform = prev.callPackage (import ./hashicorp/generic.nix) {
+    name = "terraform";
+    version = "1.2.3";
+    sha256 = "sha256-cotvvLKIrRt7ZZBYVBCpjTt+Be/kYB73dsN+FemoOpY=";
+  };
 
   # Add `--no-sandbox` flag to authy due to issues identifying GPU
   authy = prev.authy.overrideAttrs (oldAttrs: rec {
