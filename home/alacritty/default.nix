24a1d1b6e47a600507108abb61170efc114ee2bd
Paul Nicholson <brenix@gmail.com>
2022-06-26 16:29:41 -0700

    Increase font

--- home/alacritty/default.nix
+++ home/alacritty/default.nix
@@ -17,7 +17,7 @@
         family = "Hack";
         style = "Italic";
       };
-      font.size = lib.mkDefault 9;
+      font.size = lib.mkDefault 10;
       font.offset.y = -3;
       font.use_thin_strokes = false;
       draw_bold_text_with_bright_colors = false;
