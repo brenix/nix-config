24b28189b9704cf4aef2e6795ce2f2c483624282
Paul Nicholson <brenix@gmail.com>
2022-07-05 13:37:17 -0700

    Update hashicorp packages

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -5,12 +5,12 @@ final: prev: {
   dash-font = prev.callPackage ./dash-font { };
   packer = prev.callPackage (import ./hashicorp/generic.nix) {
     name = "packer";
-    version = "1.8.0";
-    sha256 = "sha256-lMXWU4b6jxfjbTSW+73xjTv6sBMGqoAAwUZWtjBwt8I=";
+    version = "1.8.2";
+    sha256 = "sha256-Z1vYJWGi5J+JdH4JIUHHznnC4qkQXmouvUmibfhJpGg=";
   };
   terraform = prev.callPackage (import ./hashicorp/generic.nix) {
     name = "terraform";
-    version = "1.2.3";
-    sha256 = "sha256-cotvvLKIrRt7ZZBYVBCpjTt+Be/kYB73dsN+FemoOpY=";
+    version = "1.2.4";
+    sha256 = "sha256-cF6mKkSgCBWU2taysJPu/vsS1U+logpmVi+eCCsAQUw=";
   };
 }
