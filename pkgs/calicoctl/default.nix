605afdfead15f7ccb86dc195d4311a30f9651c8e
Paul Nicholson <brenix@gmail.com>
2022-07-06 11:31:20 -0700

    Use binary package for calicoctl

--- pkgs/calicoctl/default.nix
+++ pkgs/calicoctl/default.nix
@@ -1,34 +1,22 @@
-{ lib, buildGoModule, fetchFromGitHub }:
+{ stdenv, fetchurl, lib }:
 
-buildGoModule rec {
+let
+  version = "3.23.2";
+in
+stdenv.mkDerivation {
   pname = "calicoctl";
-  version = "3.23.1";
+  inherit version;
 
-  src = fetchFromGitHub {
-    owner = "projectcalico";
-    repo = "calico";
-    rev = "v${version}";
-    sha256 = "sha256-h6dUKRKuiHcgN/ql1MGpQ+N1PTSki2OdIbS0j8DaQqQ=";
+  src = fetchurl {
+    url = "https://github.com/projectcalico/calico/releases/download/v${version}/calicoctl-linux-amd64";
+    sha256 = "sha256-N4QgDN/AEGyZh98gSNIZu5EUfwzD+jZbNieayC6jfHo=";
   };
 
-  vendorSha256 = "sha256-zjqbH4VIkGXjquKin52+twOxHzRH3RATcwZaQJYwCa0=";
+  phases = [ "installPhase" ];
 
-  doCheck = false;
-
-  subPackages = [ "calicoctl/calicoctl" ];
-
-  ldflags = [
-    "-s"
-    "-w"
-    "-X github.com/projectcalico/calico/calicoctl/calicoctl/commands.VERSION=${version}"
-    "-X github.com/projectcalico/calico/calicoctl/calicoctl/commands/common.VERSION=${version}"
-  ];
-
-  meta = {
-    description = "Calico CLI tool to manage network and security policy";
-    homepage = "https://github.com/projectcalico/calico";
-    license = lib.licenses.asl20;
-    /* maintainers = with lib.maintainers; [ brenix ]; */
-    platforms = lib.platforms.unix;
-  };
+  installPhase = ''
+    mkdir -p $out/bin
+    cp $src $out/bin/calicoctl
+    chmod +x $out/bin/calicoctl
+  '';
 }
