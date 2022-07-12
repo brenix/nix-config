37c8e21f5f43cb5fda06f0c3275c4754a2964307
Paul Nicholson <brenix@gmail.com>
2022-07-12 14:30:01 -0700

    Picom fixes

--- home/default.nix
+++ home/default.nix
@@ -196,11 +196,11 @@ in
         # picom
         services.picom = {
           enable = true;
-          activeOpacity = "1.0";
-          inactiveOpacity = "1.0";
+          activeOpacity = 1.0;
+          inactiveOpacity = 1.0;
           shadow = true;
           fade = false;
-          blur = false;
+          /* blur = false; */
           backend = "glx";
           /* opacityRule = [ */
           /*   "90:class_i ?= 'alacritty'" */
