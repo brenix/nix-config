990b9204c8792c860520a75a09f1ade6130b5eed
Paul Nicholson <brenix@gmail.com>
2022-08-03 09:53:17 -0700

    Add some patches

--- overlay/default.nix
+++ overlay/default.nix
@@ -4,6 +4,12 @@
   # ...
   # });
 
+  # Build waybar with experimental flag set
+  waybar = prev.waybar.overrideAttrs (oldAttrs: {
+    mesonFlags = oldAttrs.mesonFlags ++ [ "-Dexperimental=true" ];
+  });
+
+  # Add custom patch for pfetch
   pfetch = prev.pfetch.overrideAttrs (oldAttrs: {
     version = "unstable-2021-12-10";
     src = prev.fetchFromGitHub {
@@ -14,6 +20,11 @@
     };
     # Add term option, rename de to desktop, add scheme option
     patches = (oldAttrs.patches or [ ]) ++ [ ./pfetch.patch ];
+  });
+
+  # Temporary fix for https://github.com/emersion/xdg-desktop-portal-wlr/issues/216
+  xdg-desktop-portal-wlr = prev.xdg-desktop-portal-wlr.overrideAttrs (oldAttrs: rec {
+    patches = (oldAttrs.patches or [ ]) ++ [ ./fix-xdpw-hyprland-crash.patch ];
   });
 
   # Add our custom packages into the overlay
