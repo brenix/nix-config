ffa704d633029fb1162a130e10a698bba4e964e1
Paul Nicholson <brenix@gmail.com>
2022-08-17 11:37:27 -0700

    Cleanup unused fonts

--- hosts/common/optional/fonts.nix
+++ hosts/common/optional/fonts.nix
@@ -2,24 +2,15 @@
 {
   fonts.fonts = with pkgs; [
     corefonts
-    cozette
-    dejavu_fonts
-    dina-font
-    gohufont
     hack-font
-    liberation_ttf
     material-icons
     noto-fonts
-    open-sans
     roboto
-    tamzen
     terminus_font
-    ubuntu_font_family
     uw-ttyp0
-    vistafonts
     weather-icons
     (nerdfonts.override {
-      fonts = [ "Gohu" "JetBrainsMono" "RobotoMono" "UbuntuMono" ];
+      fonts = [ "JetBrainsMono" "RobotoMono" "UbuntuMono" ];
     })
   ];
 
