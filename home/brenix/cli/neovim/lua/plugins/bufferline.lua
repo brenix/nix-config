3c38ee7068a1024471d67087189c9cf8c0d2d2b5
Paul Nicholson <brenix@gmail.com>
2022-08-22 08:51:48 -0700

    Fix deprecation in bufferline

--- home/brenix/cli/neovim/lua/plugins/bufferline.lua
+++ home/brenix/cli/neovim/lua/plugins/bufferline.lua
@@ -1,25 +1,25 @@
 require("bufferline").setup({
-  options = {
-    offsets = { { filetype = "NvimTree", text = "Explorer" } },
-    always_show_bufferline = true,
-    buffer_close_icon = "",
-    modified_icon = "",
-    close_icon = "",
-    left_trunc_marker = "",
-    right_trunc_marker = "",
-    max_name_length = 18,
-    max_prefix_length = 15,
-    tab_size = 25,
-    diagnostics = "nvim_diagnostic",
-    enforce_regular_tabs = true,
-    view = "multiwindow",
-    show_buffer_close_icons = true,
-    separator_style = "thin",
-  },
-  highlights = {
-    -- Disable italic fonts
-    buffer_selected = {
-      gui = "bold",
-    },
-  },
+	options = {
+		offsets = { { filetype = "NvimTree", text = "Explorer" } },
+		always_show_bufferline = true,
+		buffer_close_icon = "",
+		modified_icon = "",
+		close_icon = "",
+		left_trunc_marker = "",
+		right_trunc_marker = "",
+		max_name_length = 18,
+		max_prefix_length = 15,
+		tab_size = 25,
+		diagnostics = "nvim_diagnostic",
+		enforce_regular_tabs = true,
+		view = "multiwindow",
+		show_buffer_close_icons = true,
+		separator_style = "thin",
+	},
+	highlights = {
+		-- Disable italic fonts
+		buffer_selected = {
+			italic = false,
+		},
+	},
 })
