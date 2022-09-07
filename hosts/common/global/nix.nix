7d3cb55482c9fb585683a9433fe2409356bc9807
Paul Nicholson <brenix@gmail.com>
2022-09-07 09:24:36 -0700

    Use cachix for hyprland

--- hosts/common/global/nix.nix
+++ hosts/common/global/nix.nix
@@ -8,9 +8,11 @@ in
     settings = {
       substituters = [
         "https://nix-community.cachix.org"
+        "https://hyprland.cachix.org"
       ];
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       ];
 
       trusted-users = [ "@wheel" ];
