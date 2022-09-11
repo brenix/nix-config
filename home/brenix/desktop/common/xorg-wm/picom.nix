c2ba7aa7f9e3cf5f1fc2d59964727453e2c10075
Paul Nicholson <brenix@gmail.com>
2022-09-11 09:13:54 -0700

    Add blur

--- home/brenix/desktop/common/xorg-wm/picom.nix
+++ home/brenix/desktop/common/xorg-wm/picom.nix
@@ -7,9 +7,17 @@
     fade = false;
     backend = "glx";
     opacityRules = [
-      "90:class_i ?= 'alacritty'"
-      "90:class_i ?= 'floating'"
-      "90:class_i ?= 'polybar'"
+      "95:class_i ?= 'alacritty'"
+      "95:class_i ?= 'floating'"
+      "95:class_i ?= 'polybar'"
     ];
+    settings = {
+      blur =
+        {
+          method = "gaussian";
+          size = 10;
+          deviation = 5.0;
+        };
+    };
   };
 }
