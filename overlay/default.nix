942b710a583188e15c5d7e350476af4721702421
Paul Nicholson <brenix@gmail.com>
2022-09-22 07:54:14 -0700

    Remove hyprland patch

--- overlay/default.nix
+++ overlay/default.nix
@@ -25,10 +25,5 @@
   # TODO: Remove once https://github.com/NixOS/nixpkgs/pull/187758 merged upstream
   piper = prev.callPackage ./piper { };
 
-  # Temporary fix for https://github.com/emersion/xdg-desktop-portal-wlr/issues/216
-  xdg-desktop-portal-wlr = prev.xdg-desktop-portal-wlr.overrideAttrs (oldAttrs: rec {
-    patches = (oldAttrs.patches or [ ]) ++ [ ./fix-xdpw-hyprland-crash.patch ];
-  });
-
   # Add our custom packages into the overlay
 } // import ../pkgs { pkgs = prev; }
