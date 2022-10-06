a9cc2283a07409904db36e72c4e3982b1aead31c
Paul Nicholson <brenix@gmail.com>
2022-10-06 08:31:38 -0700

    Update flake

--- home/brenix/desktop/common/fonts.nix
+++ home/brenix/desktop/common/fonts.nix
@@ -3,18 +3,15 @@
   fontProfiles = {
     enable = true;
     monospace = {
-      family = "Fragment Mono";
-      package = pkgs.fragment-mono;
-
-      /* family = "MesloLGS Nerd Font Mono"; */
-      /* package = pkgs.nerdfonts.override { */
-      /*   fonts = [ */
-      /*     "Meslo" */
-      /*     "Hack" */
-      /*     "JetBrainsMono" */
-      /*     "FantasqueSansMono" */
-      /*   ]; */
-      /* }; */
+      family = "MesloLGS Nerd Font Mono";
+      package = pkgs.nerdfonts.override {
+        fonts = [
+          "Meslo"
+          "Hack"
+          "JetBrainsMono"
+          "FantasqueSansMono"
+        ];
+      };
     };
     regular = {
       family = "Roboto";
