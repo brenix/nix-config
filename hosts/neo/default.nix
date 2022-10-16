eb219070b2ea96293216ca3c833dd28f0770ba03
Paul Nicholson <brenix@gmail.com>
2022-10-16 11:26:19 -0700

    Update flake, add cifs mount

--- hosts/neo/default.nix
+++ hosts/neo/default.nix
@@ -36,6 +36,19 @@
 
   services.irqbalance.enable = true;
 
+  environment.systemPackages = [ pkgs.cifs-utils ];
+  fileSystems."/mnt/share" = {
+    device = "//192.168.1.10/downloads";
+    fsType = "cifs";
+    options =
+      let
+        # this line prevents hanging on network split
+        automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+
+      in
+      [ "${automount_opts}" ];
+  };
+
   services.xserver = {
     dpi = 109;
     displayManager = {
