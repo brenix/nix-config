ce27ea3cc8f31e7d3dda0a669e28d0bbd57dabe9
Paul Nicholson <brenix@gmail.com>
2022-10-25 09:03:30 -0700

    Cleanup

--- hosts/common/optional/fonts.nix
+++ hosts/common/optional/fonts.nix
@@ -15,20 +15,17 @@
     })
   ];
 
-
   # More mac-like font rendering
   environment.sessionVariables = {
-    /* FREETYPE_PROPERTIES = "truetype:interpreter-version=35 autofitter:no-stem-darkening=0 cff:no-stem-darkening=0"; */
     FREETYPE_PROPERTIES = "truetype:interpreter-version=40 autofitter:no-stem-darkening=0 cff:no-stem-darkening=0 cff:darkening-parameters=500,500,1000,350,1667,350,2333,0";
   };
 
   fonts.fontconfig = {
-    /* hinting.style = "hintnone"; */
     defaultFonts = {
       serif = [ "Inter" ];
       sansSerif = [ "Inter" ];
       emoji = [ "Noto Color Emoji" ];
-      monospace = [ "MesloLGS Nerd Font Mono" "JetBrainsMono Nerd Font" "Roboto Mono" ];
+      monospace = [ "Hack Nerd Font Mono" "MesloLGS Nerd Font Mono" "JetBrainsMono Nerd Font" "Roboto Mono" ];
     };
   };
 }
