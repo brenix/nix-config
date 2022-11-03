5f4c0c0f222a5c3dc885059fb919a93cd67f4444
Paul Nicholson <brenix@gmail.com>
2022-11-03 16:52:40 -0700

    treesitter: use official parsers instead of nix due to isssues

--- home/brenix/features/nvim/treesitter.nix
+++ home/brenix/features/nvim/treesitter.nix
@@ -2,10 +2,14 @@
 {
   programs.neovim.plugins = with pkgs.vimPlugins; [
     {
-      plugin = nvim-treesitter.withPlugins (_: pkgs.tree-sitter.allGrammars);
+      # plugin = nvim-treesitter.withPlugins (_: pkgs.tree-sitter.allGrammars);
+      plugin = nvim-treesitter;
       type = "lua";
       config = /* lua */ ''
         require("nvim-treesitter.configs").setup {
+          ensure_installed = "all",
+          auto_install = false,
+          parser_install_dir = "/home/brenix/.local/share/nvim/site",
           highlight = {
             enable = true,
           },
