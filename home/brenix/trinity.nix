9270d502fbf7f1285514d035e642dd688e0015a7
Paul Nicholson <brenix@gmail.com>
2022-11-12 08:58:23 -0800

    Update trinity too

--- home/brenix/trinity.nix
+++ home/brenix/trinity.nix
@@ -4,6 +4,5 @@
     ./global
   ];
 
-  /* colorscheme = inputs.nix-colors.colorSchemes.catppuccin; */
-  colorscheme = inputs.nix-colors.lib-core.schemeFromYAML "nordppuccin" (builtins.readFile (./colorschemes/nordppuccin.yaml));
+  colorscheme = inputs.nix-colors.colorSchemes.nord;
 }
