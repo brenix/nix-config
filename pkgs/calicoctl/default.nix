fcd00d9e4534864205d17189c712d73622611afe
Paul Nicholson <brenix@gmail.com>
2022-11-18 10:48:32 -0800

    Update calicoctl

--- pkgs/calicoctl/default.nix
+++ pkgs/calicoctl/default.nix
@@ -1,7 +1,7 @@
 { stdenv, fetchurl, lib }:
 
 let
-  version = "3.24.3";
+  version = "3.24.5";
 in
 stdenv.mkDerivation {
   pname = "calicoctl";
@@ -9,7 +9,7 @@ stdenv.mkDerivation {
 
   src = fetchurl {
     url = "https://github.com/projectcalico/calico/releases/download/v${version}/calicoctl-linux-amd64";
-    sha256 = "sha256-Ite6VUev8bQgLd1VlSweW25F5BbNeeFyFDiqtUojMko=";
+    sha256 = "sha256-AebIojcQUPnt0K3p3N6J2gVOhNjpa9S6jPgoBsjT6Oc=";
   };
 
   phases = [ "installPhase" ];
