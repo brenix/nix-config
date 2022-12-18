6d5b713d3a453825d8aa9ea32a4f800663f050b1
Paul Nicholson <brenix@gmail.com>
2022-12-18 09:02:01 -0800

    Update flake, add gtk font tuning

--- home/brenix/features/desktop/common/gtk.nix
+++ home/brenix/features/desktop/common/gtk.nix
@@ -24,6 +24,21 @@ rec {
       package = pkgs.capitaine-cursors;
       size = 16;
     };
+
+    gtk3.extraConfig = {
+      gtk-xft-antialias = 1;
+      gtk-xft-hinting = 1;
+      gtk-xft-hintstyle = "hintslight";
+      gtk-xft-rgba = "rgb";
+      gtk-decoration-layout = "menu:";
+    };
+
+    gtk2.extraConfig = ''
+      gtk-xft-antialias=1
+      gtk-xft-hinting=1
+      gtk-xft-hintstyle="hintslight"
+      gtk-xft-rgba="rgb"
+    '';
   };
 
   services.xsettingsd = {
