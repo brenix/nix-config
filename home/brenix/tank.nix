821d797dd6b927d3aa7cc0dd54546a5f5bccf176
Paul Nicholson <brenix@gmail.com>
2023-01-26 22:07:15 -0800

    Update tank theme

--- home/brenix/tank.nix
+++ home/brenix/tank.nix
@@ -37,6 +37,6 @@
   programs.starship.settings.command_timeout = 1200;
 
   # colorscheme = inputs.nix-colors.colorSchemes.nord;
-  colorscheme = inputs.nix-colors.lib-core.schemeFromYAML "catppuccin-mocha" (builtins.readFile (./colorschemes/catppuccin-mocha.yaml));
-  wallpaper = pkgs.wallpapers.evening-sky;
+  colorscheme = inputs.nix-colors.lib-core.schemeFromYAML "nord-dark" (builtins.readFile (./colorschemes/nord-dark.yaml));
+  wallpaper = pkgs.wallpapers.mountain-jaws;
 }
