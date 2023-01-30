f98c02d371cd08c0302eca77a4ac67c0dad4d355
Paul Nicholson <brenix@gmail.com>
2023-01-30 09:01:47 -0800

    Revert "Remove zoom-us overlay"
    This reverts commit 008fba22fe1fb4ccb7170a3e5da06b44bd0dd78b.

--- overlays/default.nix
+++ overlays/default.nix
@@ -13,6 +13,16 @@
     # ...
     # });
 
+    # Downgrade zoom until it it is fixed upstream
+    zoom-us = prev.zoom-us.overrideAttrs
+      (oldAttrs: rec {
+        version = "5.13.4.711";
+        src = prev.fetchurl {
+          url = "https://zoom.us/client/${version}/zoom_x86_64.pkg.tar.xz";
+          hash = "sha256-sQk5fS/bS7e0T0IJ7+UB956XmCAbeMYfS8BVwncpoy0=";
+        };
+      });
+
     kitty = prev.kitty.overrideAttrs (oldAttrs: rec {
       doCheck = false;
       doInstallCheck = false;
