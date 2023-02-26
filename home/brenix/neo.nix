eebb9629d69f81488f925a638aa36d97e38bc089
Paul Nicholson <brenix@gmail.com>
2023-02-26 08:15:16 -0800

    update flake/wallpaper

--- home/brenix/neo.nix
+++ home/brenix/neo.nix
@@ -64,5 +64,5 @@
 
   # colorscheme = inputs.nix-colors.colorSchemes.nord;
   colorscheme = inputs.nix-colors.lib-core.schemeFromYAML "nord-dark" (builtins.readFile (./colorschemes/nord-dark.yaml));
-  wallpaper = pkgs.wallpapers.mountain-jaws;
+  wallpaper = pkgs.wallpapers.murky-peaks;
 }
