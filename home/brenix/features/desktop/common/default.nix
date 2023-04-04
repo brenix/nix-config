dc05065f1a489f1b9931a245bfb5a3ca4bfb9eeb
Paul Nicholson <brenix@gmail.com>
2023-04-03 21:00:38 -0700

    Ok try chrome again

--- home/brenix/features/desktop/common/default.nix
+++ home/brenix/features/desktop/common/default.nix
@@ -2,7 +2,7 @@
 {
   imports = [
     ./alacritty.nix
-    # ./chromium.nix
+    ./chromium.nix
     ./firefox.nix
     ./fonts.nix
     ./gtk.nix
@@ -13,14 +13,20 @@
 
   xdg.mimeApps.enable = true;
   xdg.mimeApps.defaultApplications = {
-    "text/html" = "firefox.desktop";
-    "x-scheme-handler/http" = "firefox.desktop";
-    "x-scheme-handler/https" = "firefox.desktop";
-    "x-scheme-handler/about" = "firefox.desktop";
-    "x-scheme-handler/unknown" = "firefox.desktop";
+    # "text/html" = "firefox.desktop";
+    # "x-scheme-handler/http" = "firefox.desktop";
+    # "x-scheme-handler/https" = "firefox.desktop";
+    # "x-scheme-handler/about" = "firefox.desktop";
+    # "x-scheme-handler/unknown" = "firefox.desktop";
+    "text/html" = "chromium-browser.desktop";
+    "x-scheme-handler/http" = "chromium-browser.desktop";
+    "x-scheme-handler/https" = "chromium-browser.desktop";
+    "x-scheme-handler/about" = "chromium-browser.desktop";
+    "x-scheme-handler/unknown" = "chromium-browser.desktop";
   };
 
-  home.sessionVariables.DEFAULT_BROWSER = "${pkgs.firefox}/bin/firefox";
+  # home.sessionVariables.DEFAULT_BROWSER = "${pkgs.firefox}/bin/firefox";
+  home.sessionVariables.DEFAULT_BROWSER = "${pkgs.chroimium}/bin/chromium";
 
   home.packages = with pkgs; [
     authy
