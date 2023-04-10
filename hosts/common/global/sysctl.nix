1bba8889cfea5cc2e16178e4f99b6670d67efd59
Paul Nicholson <brenix@gmail.com>
2023-04-09 20:54:11 -0700

    Update sysctl

--- hosts/common/global/sysctl.nix
+++ hosts/common/global/sysctl.nix
@@ -4,7 +4,7 @@
     kernel = {
       sysctl = {
         "net.core.default_qdisc" = "cake";
-        "net.ipv4.tcp_congestion_control" = "bbr";
+        "net.ipv4.tcp_congestion_control" = "bbr2";
         "net.ipv4.tcp_fastopen" = 3;
         "net.ipv4.tcp_slow_start_after_idle" = 0;
         "vm.dirty_background_ratio" = 5;
@@ -12,6 +12,13 @@
         "vm.min_free_kbytes" = 524288;
         "vm.swappiness" = 0;
         "vm.vfs_cache_pressure" = 10;
+
+        # https://blog.cloudflare.com/optimizing-tcp-for-high-throughput-and-low-latency/
+        "net.ipv4.tcp_rmem" = "8192 262144 536870912";
+        "net.ipv4.tcp_wmem" = "4096 16384 536870912";
+        "net.ipv4.tcp_adv_win_scale" = -2;
+        "net.ipv4.tcp_collapse_max_bytes" = 6291456;
+        "net.ipv4.tcp_notsent_lowat" = 131072;
       };
     };
   };
