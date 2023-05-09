9ce612554960bb273121546bfbae245df51bef79
Paul Nicholson <brenix@gmail.com>
2023-05-08 20:07:55 -0700

    disable chaotic repo for now

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "My NixOS configuration";
 
   inputs = rec {
-    chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
+    # chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
     firefox-addons = { url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons"; inputs.nixpkgs.follows = "nixpkgs"; };
     hardware.url = "github:nixos/nixos-hardware";
     home-manager = { url = "github:nix-community/home-manager"; inputs.nixpkgs.follows = "nixpkgs"; };
@@ -16,14 +16,15 @@
     sops-nix = { url = "github:mic92/sops-nix"; inputs.nixpkgs.follows = "nixpkgs"; };
   };
 
-  outputs = { self, nixpkgs, home-manager, chaotic, ... }@inputs:
+  outputs = { self, nixpkgs, home-manager, ... }@inputs:
     let
       inherit (self) outputs;
       forEachSystem = nixpkgs.lib.genAttrs [ "x86_64-linux" ];
       forEachPkgs = f: forEachSystem (sys: f nixpkgs.legacyPackages.${sys});
 
       mkNixos = modules: nixpkgs.lib.nixosSystem {
-        modules = modules ++ [ chaotic.nixosModules.default ];
+        inherit modules;
+        # modules = modules ++ [ chaotic.nixosModules.default ];
         specialArgs = { inherit inputs outputs; };
       };
       mkHome = modules: pkgs: home-manager.lib.homeManagerConfiguration {
