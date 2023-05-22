9f639de3ee0c474792e8a50dc380aceba7978fa2
Paul Nicholson <brenix@gmail.com>
2023-05-22 11:20:39 -0700

    Disable unused sysctl for now

--- hosts/common/global/sysctl.nix
+++ hosts/common/global/sysctl.nix
@@ -98,7 +98,7 @@
         # 1 = 32ms
         # 2 = 16ms
         # 4 = 8ms
-        "kernel.sched_pelt_multiplier" = "2";
+        # "kernel.sched_pelt_multiplier" = "2";
 
         # Increase writeback interval for xfs
         "fs.xfs.xfssyncd_centisecs" = 10000;
