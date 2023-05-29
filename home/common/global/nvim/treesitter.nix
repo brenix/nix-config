95d57fefd4656c1447457506d5d2b2b694bdf813
Paul Nicholson <brenix@gmail.com>
2023-05-28 21:14:15 -0700

    Update treesitter

--- home/common/global/nvim/treesitter.nix
+++ home/common/global/nvim/treesitter.nix
@@ -2,19 +2,14 @@
 {
   programs.neovim.plugins = with pkgs.vimPlugins; [
     {
-      # plugin = nvim-treesitter.withPlugins (_: pkgs.tree-sitter.allGrammars);
-      plugin = nvim-treesitter;
+      plugin = nvim-treesitter.withAllGrammars;
+      # plugin = nvim-treesitter;
       type = "lua";
       config = /* lua */ ''
-        local parser_install_dir = vim.fn.stdpath("cache") .. "/treesitters"
-        vim.fn.mkdir(parser_install_dir, "p")
-        vim.opt.runtimepath:append(parser_install_dir)
-
         require("nvim-treesitter.configs").setup {
-          ensure_installed = "all",
-          parser_install_dir = parser_install_dir,
           highlight = {
             enable = true,
+            additional_vim_regex_highlighting = false,
           },
           indent = {
             enable = true,
