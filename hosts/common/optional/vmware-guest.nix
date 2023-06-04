1d822da626432494a1e9d360d87952e65010c220
Paul Nicholson <brenix@gmail.com>
2023-06-04 08:49:53 -0700

    Disable host mount on vmware guest

--- hosts/common/optional/vmware-guest.nix
+++ hosts/common/optional/vmware-guest.nix
@@ -3,17 +3,18 @@
   virtualisation.vmware.guest.enable = true;
   environment.systemPackages = with pkgs; [ xorg.xf86videovmware ];
 
+  # NOTE: Disabled due to boot issue
   # Host mounts under /host
-  fileSystems."/host" = {
-    fsType = "fuse./run/current-system/sw/bin/vmhgfs-fuse";
-    device = ".host:/";
-    options = [
-      "umask=22"
-      "uid=1000"
-      "gid=1000"
-      "allow_other"
-      "auto_unmount"
-      "defaults"
-    ];
-  };
+  # fileSystems."/host" = {
+  #   fsType = "fuse./run/current-system/sw/bin/vmhgfs-fuse";
+  #   device = ".host:/";
+  #   options = [
+  #     "umask=22"
+  #     "uid=1000"
+  #     "gid=1000"
+  #     "allow_other"
+  #     "auto_unmount"
+  #     "defaults"
+  #   ];
+  # };
 }
