18dc6badf0b5b02d3661572f65db3e376489f0de
Paul Nicholson <brenix@gmail.com>
2023-08-01 15:01:49 -0700

    Update terraform-ls temporarily

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,6 +2,8 @@
   awless = pkgs.callPackage ./awless { };
   calicoctl = pkgs.callPackage ./calicoctl { };
   monaco = pkgs.callPackage ./monaco { };
+  # TODO: Add updated version of terraform-ls until merged upstream
+  terraform-ls = pkgs.callPackage ./terraform-ls { };
   tfenv = pkgs.callPackage ./tfenv { };
   tfsort = pkgs.callPackage ./tfsort { };
   wootility-lekker = pkgs.callPackage ./wootility-lekker { };
