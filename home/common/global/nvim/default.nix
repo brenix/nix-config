526f1d5c1588fdd619928f19107e8369fddff2fa
Paul Nicholson <brenix@gmail.com>
2023-08-06 08:45:19 -0700

    Fix nvim-cmp

--- home/common/global/nvim/default.nix
+++ home/common/global/nvim/default.nix
@@ -45,6 +45,7 @@
 
     extraFiles = {
       "colors/nix-${config.colorscheme.slug}.vim" = import ./theme.nix config.colorscheme;
+      
     };
 
     # -- COLORSCHEME -- #
@@ -113,13 +114,15 @@
             action = ''
               function(fallback)
                 if cmp.visible() then
-                  cmp.select_next_item()
-                elseif has_words_before() then
-                  cmp.complete()
-                else
-                  fallback()
-                end
+                cmp.select_next_item()
+              elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+              elseif has_words_before() then
+                cmp.complete()
+              else
+                fallback()
               end
+            end
             '';
           };
           "<S-Tab>" = {
@@ -128,6 +131,8 @@
               function(fallback)
                 if cmp.visible() then
                   cmp.select_prev_item()
+                elseif luasnip.jumpable(-1) then
+                  luasnip.jump(-1)
                 else
                   fallback()
                 end
@@ -209,6 +214,7 @@
       nvim-tree = {
         enable = true;
         reloadOnBufenter = true;
+        actions.changeDir.restrictAboveCwd = true;
         renderer = {
           highlightGit = true;
           specialFiles = [ "" ];
@@ -225,14 +231,32 @@
 
     extraPlugins = with pkgs.vimPlugins;  [
       comment-box-nvim
-      nvim-bufdel
-      vim-better-whitespace
+      luasnip
       mkdir-nvim
+      nvim-bufdel
       quickfix-reflector-vim
       switch-vim
+      vim-better-whitespace
       vim-helm
     ];
 
+    extraConfigLuaPre = ''
+      luasnip = require'luasnip'
+      mycmp = require'cmp'
+      cmp_autopairs = require('nvim-autopairs.completion.cmp')
+
+      function has_words_before()
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
+      end
+
+      mycmp.setup({
+        confirm_opts = {
+          behavior = "Replace",
+          select = false,
+        }
+      })
+    '';
 
     # -- MISC -- #
     editorconfig.enable = true;
