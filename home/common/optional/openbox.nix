821c3d303c4e1f47b0b539937294b9a181534659
Paul Nicholson <brenix@gmail.com>
2023-08-09 08:11:43 -0700

    Update flake; fix wallpaper; disable infinality patch

--- home/common/optional/openbox.nix
+++ home/common/optional/openbox.nix
@@ -8,7 +8,7 @@ in
   ];
 
   xdg.configFile."openbox/autostart".text = ''
-    "${pkgs.feh}/bin/feh --bg-scale ~/downloads/wallpaper.jpg"
+    ${pkgs.feh}/bin/feh --bg-scale ~/downloads/wallpaper.jpg
   '';
 
   xdg.configFile."openbox/rc.xml".text = ''
