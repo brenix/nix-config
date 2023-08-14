476213641f09f9e8faaeae483936b335a7f3cf6c
Paul Nicholson <brenix@gmail.com>
2023-08-14 10:30:27 -0700

    autocutsel: attempt to fix issue

--- home/common/optional/desktop/autocutsel.nix
+++ home/common/optional/desktop/autocutsel.nix
@@ -9,6 +9,7 @@
       Type = "forking";
       Restart = "always";
       RestartSec = 2;
+      ExecStartPre = "${pkgs.autocutsel}/bin/autocutsel -fork";
       ExecStart = "${pkgs.autocutsel}/bin/autocutsel -selection PRIMARY -fork";
     };
 
