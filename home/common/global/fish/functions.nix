8ebfbc3261f4891c18467e2ae19e2e56485c307c
Paul Nicholson <brenix@gmail.com>
2023-10-17 15:42:45 -0700

    Fix ssh-nodes function

--- home/common/global/fish/functions.nix
+++ home/common/global/fish/functions.nix
@@ -229,7 +229,7 @@
 
   # ssh to multiple k8s nodes in tmux panes
   ssh-nodes = ''
-    set -l hosts (string split " " (kubectl get nodes -o jsonpath=(string escape '{.items[*].status.addresses[?(@.type=="InternalIP")].address}') "$argv"))
+    set -l hosts (string split " " (kubectl get nodes -o jsonpath='{.items[*].status.addresses[?(@.type=="InternalIP")].address}' "$argv"))
     ssh-multi $hosts
   '';
 }
