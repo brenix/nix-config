0d7a8b6db9c90c69dcb49fa451a629e45e578417
Paul Nicholson <brenix@gmail.com>
2023-10-26 18:35:14 -0700

    Update flake

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,7 +2,6 @@
   awless = pkgs.callPackage ./awless { };
   calicoctl = pkgs.callPackage ./calicoctl { };
   monaco = pkgs.callPackage ./monaco { };
-  helix = pkgs.callPackage ./helix { };
   tfenv = pkgs.callPackage ./tfenv { };
   tfsort = pkgs.callPackage ./tfsort { };
   wootility-lekker = pkgs.callPackage ./wootility-lekker { };
