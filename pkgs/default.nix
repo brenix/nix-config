a2fb2473f9d8dca57ac56fef5be24e9acb2aa379
Paul Nicholson <brenix@gmail.com>
2023-11-19 10:33:36 -0800

    Various fixes

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,8 +1,8 @@
 { pkgs ? import <nixpkgs> { } }: {
   awless = pkgs.callPackage ./awless { };
+  awscli2 = pkgs.callPackage ./awscli2 { }; # FIXME: remove once fixed upstream: https://github.com/NixOS/nixpkgs/pull/267878
   calicoctl = pkgs.callPackage ./calicoctl { };
   monaco = pkgs.callPackage ./monaco { };
-  monaspace = pkgs.callPackage ./monaspace { };
   tfenv = pkgs.callPackage ./tfenv { };
   tfsort = pkgs.callPackage ./tfsort { };
   wootility-lekker = pkgs.callPackage ./wootility-lekker { };
