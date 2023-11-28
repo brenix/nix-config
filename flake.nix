72ba8b3ffac1ce555c431dce7939bf8eda9a8495
Paul Nicholson <brenix@gmail.com>
2023-11-28 07:58:19 -0800

    Switch back to unstable

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
     impermanence.url = "github:nix-community/impermanence";
     # neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
     nix-colors.url = "github:misterio77/nix-colors";
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixvim.url = "github:nix-community/nixvim";
     nur.url = "github:nix-community/NUR";
     sops-nix = { url = "github:mic92/sops-nix"; inputs.nixpkgs.follows = "nixpkgs"; };
