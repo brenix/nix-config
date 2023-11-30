d17d9e674e7daa08128502b8ecfd8373673a3869
Paul Nicholson <brenix@gmail.com>
2023-11-30 15:42:16 -0800

    Add hipreport script for openconnect

--- hosts/common/optional/openconnect.nix
+++ hosts/common/optional/openconnect.nix
@@ -8,6 +8,8 @@
       text = "resolvectl default-route \${TUNDEV} false";
       mode = "0755";
     };
+
+    "openconnect/hipreport.sh".source = "${pkgs.openconnect}/libexec/openconnect/hipreport.sh";
   };
 
 }
