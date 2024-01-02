a793ed1d3951a6908093b8614ad8ad03ddb7f503
Paul Nicholson <brenix@gmail.com>
2024-01-02 14:33:53 -0800

    Remove font rendering patch to eval as pure

--- Makefile
+++ Makefile
@@ -2,15 +2,15 @@ HOSTNAME ?= $(shell hostname)
 
 ## Rebuild NixOS configuration
 nixos:
-	@sudo nixos-rebuild switch --verbose --impure --flake ".#$(HOSTNAME)"
+	@sudo nixos-rebuild switch --verbose --flake ".#$(HOSTNAME)"
 
 ## Rebuild and upgrade NixOS configuration
 upgrade:
-	@sudo nixos-rebuild switch --verbose --impure --upgrade-all --recreate-lock-file --flake ".#$(HOSTNAME)"
+	@sudo nixos-rebuild switch --verbose --upgrade-all --recreate-lock-file --flake ".#$(HOSTNAME)"
 
 ## Generate ISO
 iso:
-	@sudo sudo nix build --impure .#nixosConfigurations.iso.config.system.build.isoImage
+	@sudo sudo nix build .#nixosConfigurations.iso.config.system.build.isoImage
 
 ## Run garbage collection
 gc:
--- nixos/optional/fonts.nix
+++ nixos/optional/fonts.nix
@@ -35,24 +35,28 @@
   };
 
   # More mac-like font rendering
+  # interpreter-version:
+  #   35=old freetype, which makes fonts fatter
+  #   38=infinality
+  #   40=new freetype
   environment.sessionVariables = {
-    FREETYPE_PROPERTIES = "truetype:interpreter-version=40 autofitter:no-stem-darkening=0 cff:no-stem-darkening=0 cff:darkening-parameters=500,500,1000,350,1667,350,2333,0";
+    FREETYPE_PROPERTIES = "truetype:interpreter-version=35 autofitter:no-stem-darkening=0 autofitter:darkening-parameters=500,450,1000,325,1667,325,2333,0 cff:darkening-parameters=500,450,1000,325,1667,325,2333,0 cff:no-stem-darkening=0";
   };
 
   # Replace freetype with patched lcdfilter - causes impure evaluation
-  system.replaceRuntimeDependencies = with pkgs; [{
-    original = freetype;
-    replacement = pkgs.lib.overrideDerivation freetype (oldAttrs: {
-      patches = oldAttrs.patches ++ [
-        (fetchurl {
-          url = "https://aur.archlinux.org/cgit/aur.git/plain/like-ultimate5.patch?h=freetype2-ultimate5";
-          sha256 = "sha256-lVTTojYZpGz0jlEuW2M2r9CAL0L9r5suR7oMcYFD8d0=";
-        })
-        # (fetchurl {
-        #   url = "https://raw.githubusercontent.com/archlinux/svntogit-packages/packages/freetype2/trunk/0003-Enable-infinality-subpixel-hinting.patch";
-        #   sha256 = "sha256-jmHRLr27y3ZKONR5jucoB0usCqIJeNU4tucEWmOUmrg=";
-        # })
-      ];
-    });
-  }];
+  # system.replaceRuntimeDependencies = with pkgs; [{
+  #   original = freetype;
+  #   replacement = pkgs.lib.overrideDerivation freetype (oldAttrs: {
+  #     patches = oldAttrs.patches ++ [
+  #       (fetchurl {
+  #         url = "https://aur.archlinux.org/cgit/aur.git/plain/like-ultimate5.patch?h=freetype2-ultimate5";
+  #         sha256 = "sha256-lVTTojYZpGz0jlEuW2M2r9CAL0L9r5suR7oMcYFD8d0=";
+  #       })
+  #       # (fetchurl {
+  #       #   url = "https://raw.githubusercontent.com/archlinux/svntogit-packages/packages/freetype2/trunk/0003-Enable-infinality-subpixel-hinting.patch";
+  #       #   sha256 = "sha256-jmHRLr27y3ZKONR5jucoB0usCqIJeNU4tucEWmOUmrg=";
+  #       # })
+  #     ];
+  #   });
+  # }];
 }
