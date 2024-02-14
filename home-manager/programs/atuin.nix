ac5312e74ffd63517de60ca566615d4055e7afbc
Paul Nicholson <brenix@gmail.com>
2024-02-14 15:09:06 -0800

    atuin: persist local data

--- home-manager/programs/atuin.nix
+++ home-manager/programs/atuin.nix
@@ -14,4 +14,13 @@
   sops.secrets.atuin_key = {
     sopsFile = ../secrets.yaml;
   };
+
+  home.persistence = {
+    "/persist/home/brenix" = {
+      directories = [
+        ".local/share/atuin"
+      ];
+      allowOther = true;
+    };
+  };
 }
