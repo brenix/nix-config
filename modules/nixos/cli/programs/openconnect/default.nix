af991c9ca54960fb1f7ca1009517175bdaca13d8
Paul Nicholson <brenix@gmail.com>
2024-04-20 01:02:49 -0700

    feat(modules/nixos): add cli tools

---
+++ modules/nixos/cli/programs/openconnect/default.nix
@@ -0,0 +1,29 @@
+{ config
+, lib
+, ...
+}:
+with lib;
+with lib.nixicle; let
+  cfg = config.cli.programs.openconnect;
+in
+{
+  options.cli.programs.openconnect = with types; {
+    enable = mkBoolOpt false "Whether or not to enable openconnect.";
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [
+      openconnect
+    ];
+
+    # Split DNS queries
+    environment.etc = mkIf config.services.systemd.resolved {
+      "vpnc/post-connect.d/resolved" = {
+        text = "resolvectl default-route \${TUNDEV} false";
+        mode = "0755";
+      };
+
+      "openconnect/hipreport.sh".source = "${pkgs.openconnect}/libexec/openconnect/hipreport.sh";
+    };
+  };
+}
