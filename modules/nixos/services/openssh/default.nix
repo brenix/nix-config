50e9c3674b4b219db05d10d61063882e7aa40b5c
Paul Nicholson <brenix@gmail.com>
2024-04-19 23:22:05 -0700

    feat(modules/nixos): add some services

---
+++ modules/nixos/services/openssh/default.nix
@@ -0,0 +1,37 @@
+{ config
+, lib
+, format ? ""
+, ...
+}:
+with lib;
+with lib.nixicle; let
+  cfg = config.services.ssh;
+in
+{
+  options.services.ssh = with types; {
+    enable = mkBoolOpt false "Enable ssh";
+    authorizedKeys = mkOpt (listOf str) [ ] "The public keys to apply.";
+  };
+
+  config = mkIf cfg.enable {
+    services.openssh = {
+      enable = true;
+      ports = [ 22 ];
+
+      settings = {
+        PasswordAuthentication = false;
+        PermitRootLogin =
+          if format == "install-iso"
+          then "yes"
+          else "no";
+        StreamLocalBindUnlink = "yes";
+        GatewayPorts = "clientspecified";
+      };
+    };
+    users.users = {
+      ${config.user.name}.openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG++dlRrheRZgVLtzadOWFJgHgEL27t70oUZyLwL1o0F brenix@nixos"
+      ];
+    };
+  };
+}
---
+++ modules/nixos/services/virtualisation/kvm/default.nix
@@ -0,0 +1,76 @@
+{ lib
+, pkgs
+, config
+, ...
+}:
+with lib;
+with lib.nixicle; let
+  cfg = config.services.virtualisation.kvm;
+in
+{
+  options.services.virtualisation.kvm = {
+    enable = lib.mkEnableOption "Enable kvm virtualisation";
+  };
+
+  config = lib.mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [
+      libguestfs
+      win-virtio
+      win-spice
+      virt-manager
+      virt-viewer
+    ];
+
+    environment.persist.directories = [
+      "/var/lib/libvirt"
+    ];
+
+    virtualisation = {
+      spiceUSBRedirection.enable = true;
+
+      libvirtd = {
+        enable = true;
+        allowedBridges = [
+          "nm-bridge"
+          "virbr0"
+        ];
+        onBoot = "ignore";
+        onShutdown = "shutdown";
+        qemu = {
+          # Don't hook evdev at vm start
+          package = pkgs.qemu_kvm.overrideAttrs (old: {
+            patches =
+              old.patches
+              ++ [
+                (pkgs.writeText "qemu.diff" ''
+                  diff --git a/ui/input-linux.c b/ui/input-linux.c
+                  index e572a2e..a9d76ba 100644
+                  --- a/ui/input-linux.c
+                  +++ b/ui/input-linux.c
+                  @@ -397,12 +397,6 @@ static void input_linux_complete(UserCreatable *uc, Error **errp)
+                       }
+
+                       qemu_set_fd_handler(il->fd, input_linux_event, NULL, il);
+                  -    if (il->keycount) {
+                  -        /* delay grab until all keys are released */
+                  -        il->grab_request = true;
+                  -    } else {
+                  -        input_linux_toggle_grab(il);
+                  -    }
+                       QTAILQ_INSERT_TAIL(&inputs, il, next);
+                       il->initialized = true;
+                       return;
+                '')
+              ];
+          });
+          runAsRoot = false;
+          swtpm.enable = true;
+          ovmf = {
+            enable = true;
+            packages = [ pkgs.OVMFFull.fd ];
+          };
+        };
+      };
+    };
+  };
+}
