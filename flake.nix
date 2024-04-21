f472605696b1dccedb6e0eb47e2bb4674b57932c
Paul Nicholson <brenix@gmail.com>
2024-04-21 09:59:27 -0700

    feat(flake): add flake config

---
+++ flake.nix
@@ -0,0 +1,139 @@
+{
+  description = "brenix's Nix/NixOS Config";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    snowfall-lib = {
+      url = "github:snowfallorg/lib";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    disko.url = "github:nix-community/disko";
+    hardware.url = "github:nixos/nixos-hardware";
+    sops-nix.url = "github:mic92/sops-nix";
+    chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
+
+    impermanence.url = "github:nix-community/impermanence";
+    lanzaboote.url = "github:nix-community/lanzaboote";
+
+    nixgl.url = "github:nix-community/nixGL";
+    nix-colors.url = "github:misterio77/nix-colors";
+    catppuccin.url = "github:catppuccin/nix";
+    nix-ld.url = "github:Mic92/nix-ld";
+    nix-index-database.url = "github:nix-community/nix-index-database";
+
+    nixos-generators = {
+      url = "github:nix-community/nixos-generators";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    comma = {
+      url = "github:nix-community/comma";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    hypr-contrib = {
+      url = "github:hyprwm/Hyprcursor";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    hyprcursor = {
+      url = "github:hyprwm/Hyprcursor";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    hypridle = {
+      url = "github:hyprwm/Hypridle";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    hyprlock = {
+      url = "github:hyprwm/Hyprlock";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    hyprpaper = {
+      url = "github:hyprwm/hyprpaper";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    pyprland = {
+      url = "github:hyprland-community/pyprland";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    hyprland-git.url = "github:hyprwm/hyprland";
+    hyprland-xdph-git.url = "github:hyprwm/xdg-desktop-portal-hyprland";
+    hyprland-protocols-git.url = "github:hyprwm/xdg-desktop-portal-hyprland";
+    hyprland-nix.url = "github:spikespaz/hyprland-nix";
+    hyprland-nix.inputs = {
+      hyprland.follows = "hyprland-git";
+      hyprland-xdph.follows = "hyprland-xdph-git";
+      hyprland-protocols.follows = "hyprland-protocols-git";
+    };
+
+    # nixvim = {
+    #   url = "github:pta2002/nixvim";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
+
+    # attic = {
+    #   url = "github:zhaofengli/attic";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
+
+    # For waiting for updates
+    waybar.url = "github:Alexays/Waybar?rev=32eac3ccb738691974121b77b4af0c47d1cbe524";
+
+    firefox-gnome-theme = {
+      url = "github:rafaelmardojai/firefox-gnome-theme";
+      flake = false;
+    };
+  };
+
+  outputs = inputs:
+    inputs.snowfall-lib.mkFlake {
+      inherit inputs;
+      src = ./.;
+      snowfall = {
+        metadata = "nixicle";
+        namespace = "nixicle";
+        meta = {
+          name = "nixicle";
+          title = "brenix's Nix Flake";
+        };
+      };
+
+      channels-config = {
+        allowUnfree = true;
+      };
+
+      systems.modules.nixos = with inputs; [
+        home-manager.nixosModules.home-manager
+        disko.nixosModules.disko
+        lanzaboote.nixosModules.lanzaboote
+        impermanence.nixosModules.impermanence
+        sops-nix.nixosModules.sops
+        nix-ld.nixosModules.nix-ld
+        catppuccin.nixosModules.catppuccin
+      ];
+
+      systems.hosts.framework.modules = with inputs; [
+        hardware.nixosModules.framework-13-7040-amd
+      ];
+
+      # homes.modules = with inputs; [
+      # impermanence.nixosModules.home-manager.impermanence
+      # ];
+
+      overlays = with inputs; [
+        nixgl.overlay
+      ];
+    };
+}
