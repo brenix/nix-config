80fbc625a69f487541c87dc97773bd2d0fa203f4
Paul Nicholson <brenix@gmail.com>
2024-04-21 09:30:29 -0700

    feat(modules/home): add sops

---
+++ modules/home/security/sops/default.nix
@@ -0,0 +1,31 @@
+{ config
+, lib
+, inputs
+, ...
+}:
+with lib;
+with lib.nixicle; let
+  cfg = config.security.sops;
+in
+{
+  options.security.sops = with types; {
+    enable = mkBoolOpt false "Whether to enable sop for secrets management.";
+  };
+
+  imports = with inputs; [
+    sops-nix.homeManagerModules.sops
+  ];
+
+  config = mkIf cfg.enable {
+    sops = {
+      age = {
+        generateKey = true;
+        keyFile = "/home/${config.nixicle.user.name}/.config/sops/age/keys.txt";
+        sshKeyPaths = [ "/home/${config.nixicle.user.name}/.ssh/id_ed25519" ];
+      };
+
+      defaultSymlinkPath = "/run/user/1000/secrets";
+      defaultSecretsMountPoint = "/run/user/1000/secrets.d";
+    };
+  };
+}
