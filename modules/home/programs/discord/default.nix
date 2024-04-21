8ae3334ec40f900ba95ba4842401daf7427f748d
Paul Nicholson <brenix@gmail.com>
2024-04-21 09:24:30 -0700

    feat(modules/home): add discord

---
+++ modules/home/programs/discord/default.nix
@@ -0,0 +1,19 @@
+{ config
+, lib
+, pkgs
+, ...
+}:
+with lib; let
+  cfg = config.programs.discord;
+in
+{
+  options.programs.discord = {
+    enable = mkEnableOption "Enable discord program";
+  };
+
+  config = mkIf cfg.enable {
+    home.packages = with pkgs; [
+      discord-krisp # chaotic-nyx
+    ];
+  };
+}
