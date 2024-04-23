b0cce2e26999a77f696049d5bb110b5fefc51f5f
Paul Nicholson <brenix@gmail.com>
2024-04-23 07:54:40 -0700

    fix(waybar): Update style

--- modules/home/desktops/addons/waybar/default.nix
+++ modules/home/desktops/addons/waybar/default.nix
@@ -18,24 +18,28 @@ in {
         {
           layer = "top";
           position = "top";
+          height = 13;
           margin = "0 0 0 0";
           modules-left = [
             "hyprland/workspaces"
-            "tray"
           ];
           modules-center = [
-            "custom/notification"
-            "clock"
-            "idle_inhibitor"
+            "custom/nowplaying"
           ];
           modules-right = [
+            "pulseaudio"
+            "cpu"
+            "memory"
+            "temperature"
             "backlight"
             "battery"
-            "pulseaudio"
             "network"
+            "clock"
+            "tray"
           ];
           "hyprland/workspaces" = {
-            format = "{icon}";
+            # format = "{icon}";
+            format = "{name}";
             sort-by-number = true;
             active-only = false;
             format-icons = {
@@ -52,8 +56,22 @@ in {
             };
             on-click = "activate";
           };
+          "custom/nowplaying" = {
+            exec-if = "playerctl status";
+            exec = ''playerctl metadata --format '{"text": "{{artist}} - {{title}}", "alt": "{{status}}", "tooltip": "{{title}} ({{artist}} - {{album}})"}' '';
+            return-type = "json";
+            interval = 1;
+            max-length = 30;
+            format = "{icon} {}";
+            format-icons = {
+              "Playing" = "";
+              "Paused" = "";
+              "Stopped" = "";
+            };
+            on-click = "playerctl play-pause";
+          };
           clock = {
-            format = "󰃰 {:%a, %d %b, %I:%M %p}";
+            format = "󰃰 {:%Y-%m-%d %I:%M %p}";
             interval = 1;
             tooltip-format = "<tt><small>{calendar}</small></tt>";
             calendar = {
@@ -91,13 +109,6 @@ in {
             "on-click-right" = "sleep 0.1 && swaync-client -d -sw";
             escape = true;
           };
-          "idle_inhibitor" = {
-            format = "{icon}";
-            format-icons = {
-              activated = "  ";
-              deactivated = "  ";
-            };
-          };
           backlight = {
             format = " {percent}%";
           };
@@ -111,6 +122,26 @@ in {
             format-alt = "{time}";
             format-charging = "  {capacity}%";
             format-icons = ["󰁻 " "󰁽 " "󰁿 " "󰂁 " "󰂂 "];
+          };
+          temperature = {
+            interval = 1;
+            hwmon-path = [ "/sys/class/hwmon/hwmon4/temp1_input" "/sys/class/hwmon/hwmon1/temp1_input" ];
+            tooltip = false;
+            thermal-zone = 1;
+            critical-threshold = 80;
+            format = "{icon} {temperatureC}°C";
+            format-critical = "{icon} {temperatureC}°C";
+            format-icons = [ "" "" "" "" "" ];
+          };
+          cpu = {
+            interval = 1;
+            tooltip = false;
+            format = " {usage}%";
+          };
+          memory = {
+            interval = 30;
+            format = " {used:0.1f}GiB";
+            tooltip-format = "{used:0.1f}GiB/{total:0.1f}GiB";
           };
           network = {
             interval = 1;
