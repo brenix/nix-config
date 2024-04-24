f3653953d9ccbe4ee72958bfa9eb50035bd793d5
Paul Nicholson <brenix@gmail.com>
2024-04-23 20:46:34 -0700

    feat(macbook): add home profile

---
+++ homes/aarch64-darwin/pnicholson@macbook/default.nix
@@ -0,0 +1,20 @@
+{
+  nixicle.user = {
+    enable = true;
+    name = "pnicholson";
+  };
+
+  profiles = {
+    common.enable = true;
+  };
+
+  programs = {
+    cli.go.enable = true;
+  };
+
+  home.sessionPath = [
+    "$HOME/bin"
+  ];
+
+  home.stateVersion = "23.11";
+}
