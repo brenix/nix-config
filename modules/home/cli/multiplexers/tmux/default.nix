a729a3b26fa453630ae273ec49728af293f8917d
Paul Nicholson <brenix@gmail.com>
2024-04-23 21:29:56 -0700

    fix(tmux): set window number to the left

--- modules/home/cli/multiplexers/tmux/default.nix
+++ modules/home/cli/multiplexers/tmux/default.nix
@@ -35,9 +35,9 @@ in {
           extraConfig = ''
             set -g @catppuccin_flavour 'mocha'
             set -g @catppuccin_window_left_separator "█"
-            set -g @catppuccin_window_right_separator "█ "
-            set -g @catppuccin_window_middle_separator " █"
-            set -g @catppuccin_window_number_position "right"
+            set -g @catppuccin_window_right_separator "█"
+            set -g @catppuccin_window_middle_separator "█ "
+            set -g @catppuccin_window_number_position "left"
 
             set -g @catppuccin_window_default_fill "number"
             set -g @catppuccin_window_default_text "#W"
