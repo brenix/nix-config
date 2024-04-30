2faf7ee5a00d49cce0a6f27f617cc262b0ea5031
Paul Nicholson <brenix@gmail.com>
2024-04-30 08:03:20 -0700

    fix(foot): colors

--- modules/home/cli/terminals/foot/default.nix
+++ modules/home/cli/terminals/foot/default.nix
@@ -7,6 +7,7 @@
 with lib;
 with lib.matrix; let
   cfg = config.cli.terminals.foot;
+  inherit (config.colorscheme) palette;
 in {
   options.cli.terminals.foot = with types; {
     enable = mkBoolOpt false "enable foot terminal emulator";
@@ -15,7 +16,7 @@ in {
   config = mkIf cfg.enable {
     programs.foot = {
       enable = true;
-      catppuccin.enable = true;
+      # catppuccin.enable = true;
 
       settings = {
         main = {
@@ -25,6 +26,26 @@ in {
           line-height = "18px";
           shell = "${pkgs.fish}/bin/fish";
           selection-target = "primary";
+        };
+        colors = {
+          foreground = palette.base05;
+          background = palette.base00;
+          regular0 = palette.base02;
+          regular1 = palette.base08;
+          regular2 = palette.base0B;
+          regular3 = palette.base0A;
+          regular4 = palette.base0D;
+          regular5 = palette.base0E;
+          regular6 = palette.base0C;
+          regular7 = palette.base05;
+          bright0 = palette.base03;
+          bright1 = palette.base08;
+          bright2 = palette.base0B;
+          bright3 = palette.base0A;
+          bright4 = palette.base0D;
+          bright5 = palette.base0E;
+          bright6 = palette.base0C;
+          bright7 = palette.base06;
         };
 
         scrollback = {
