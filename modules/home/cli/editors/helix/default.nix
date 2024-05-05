7afe861356ef51667e6f89a41f26413bd7b9c430
Paul Nicholson <brenix@gmail.com>
2024-05-05 10:33:47 -0700

    fix(helix): configure yaml schemas

--- modules/home/cli/editors/helix/default.nix
+++ modules/home/cli/editors/helix/default.nix
@@ -130,6 +130,44 @@ in {
             command = "${nodePackages.vscode-json-languageserver-bin}/bin/json-languageserver";
             args = ["--stdio"];
           };
+          yaml-language-server = {
+            config.yaml = {
+              completion = true;
+              validation = true;
+              hover = true;
+              # schemaStore.enable = true;
+            };
+            config.yaml.schemas = {
+              # ansible tasks
+              "https://raw.githubusercontent.com/ansible/ansible-lint/main/src/ansiblelint/schemas/ansible.json#/$defs/tasks" = "tasks/*.{yml,yaml}";
+              # ansible playbooks
+              "https://raw.githubusercontent.com/ansible/ansible-lint/main/src/ansiblelint/schemas/ansible.json#/$defs/playbook" = "*ansible*/*.{yml,yaml}";
+              # kustomization
+              "http://json.schemastore.org/kustomization" = "kustomization.{yml,yaml}";
+              # helm chart
+              "http://json.schemastore.org/chart" = "Chart.{yml,yaml}";
+              # helmfile
+              "http://json.schemastore.org/helmfile.json" = "helmfile.{yml,yaml}";
+              # gitlab-ci
+              "https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json" = "*gitlab-ci*.{yml,yaml}";
+              # argo workflows
+              "https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json" = "*flow*.{yml,yaml}";
+              # taskfile
+              "http://json.schemastore.org/taskfile.json" = "Taskfile*.{yml,yaml}";
+              # kubernetes
+              "https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/v1.30.0/all.json" = "/*.yaml";
+              # github workflow
+              "http://json.schemastore.org/github-workflow" = ".github/workflows/*";
+              # github actions
+              "http://json.schemastore.org/github-action" = ".github/action.{yml,yaml}";
+              # flux kustomization
+              "https://raw.githubusercontent.com/fluxcd-community/flux2-schemas/main/kustomization-kustomize-v1.json" = "ks.{yml,yaml}";
+              # flux helmrelease
+              "https://kubernetes-schemas.pages.dev/helm.toolkit.fluxcd.io/helmrelease_v2beta2.json" = "helmrelease.{yml,yaml}";
+              # externalsecrets
+              "https://raw.githubusercontent.com/datreeio/CRDs-catalog/main/external-secrets.io/externalsecret_v1beta1.json" = "external*secret.{yml,yaml}";
+            };
+          };
         };
         language = [
           {
