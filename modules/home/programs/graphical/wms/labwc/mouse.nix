d088d5a005bfa8a0fcb6f4e2168d5502bf48257b
Paul Nicholson <brenix@gmail.com>
2024-09-27 18:50:46 -0700

    update mousebinds

--- modules/home/programs/graphical/wms/labwc/mouse.nix
+++ modules/home/programs/graphical/wms/labwc/mouse.nix
@@ -1,6 +1,6 @@
 {
   programs.labwc.config.mouse = {
-    default = false;
+    default = true;
     mousebinds = {
       "Root" = [
         {
@@ -9,38 +9,6 @@
           actions = [
             {
               name = "None";
-            }
-          ];
-        }
-        {
-          action = "Press";
-          button = "Right";
-          actions = [
-            {
-              name = "ShowMenu";
-              menu = "root-menu";
-            }
-          ];
-        }
-        {
-          action = "Scroll";
-          direction = "Up";
-          actions = [
-            {
-              name = "GoToDesktop";
-              to = "left";
-              wrap = "yes";
-            }
-          ];
-        }
-        {
-          action = "Scroll";
-          direction = "Down";
-          actions = [
-            {
-              name = "GoToDesktop";
-              to = "right";
-              wrap = "yes";
             }
           ];
         }
@@ -68,32 +36,9 @@
       ];
 
       "Title" = [
+        # Disable context menu on right-click
         {
-          action = "Drag";
-          button = "Left";
-          actions = [
-            {
-              name = "Move";
-            }
-          ];
-        }
-      ];
-
-      "TitleBar" = [
-        {
-          action = "Press";
-          button = "Left";
-          actions = [
-            {
-              name = "Focus";
-            }
-            {
-              name = "Raise";
-            }
-          ];
-        }
-        {
-          action = "Press";
+          action = "Click";
           button = "Right";
           actions = [
             {
@@ -101,6 +46,9 @@
             }
           ];
         }
+      ];
+
+      "TitleBar" = [
         {
           action = "Press";
           button = "Right";
@@ -125,271 +73,6 @@
           actions = [
             {
               name = "Shade";
-            }
-          ];
-        }
-      ];
-
-      "Top" = [
-        {
-          action = "Drag";
-          button = "Left";
-          actions = [
-            {
-              name = "Resize";
-            }
-          ];
-        }
-      ];
-
-      "Left" = [
-        {
-          action = "Drag";
-          button = "Left";
-          actions = [
-            {
-              name = "Resize";
-            }
-          ];
-        }
-      ];
-
-      "Right" = [
-        {
-          action = "Drag";
-          button = "Left";
-          actions = [
-            {
-              name = "Resize";
-            }
-          ];
-        }
-      ];
-
-      "Bottom" = [
-        {
-          action = "Drag";
-          button = "Left";
-          actions = [
-            {
-              name = "Resize";
-            }
-          ];
-        }
-      ];
-
-      "TRCorner" = [
-        {
-          action = "Drag";
-          button = "Left";
-          actions = [
-            {
-              name = "Resize";
-            }
-          ];
-        }
-      ];
-
-      "BRCorner" = [
-        {
-          action = "Drag";
-          button = "Left";
-          actions = [
-            {
-              name = "Resize";
-            }
-          ];
-        }
-      ];
-
-      "TLCorner" = [
-        {
-          action = "Drag";
-          button = "Left";
-          actions = [
-            {
-              name = "Resize";
-            }
-          ];
-        }
-      ];
-
-      "BLCorner" = [
-        {
-          action = "Drag";
-          button = "Left";
-          actions = [
-            {
-              name = "Resize";
-            }
-          ];
-        }
-      ];
-
-      "Maximize" = [
-        {
-          action = "Click";
-          button = "Left";
-          actions = [
-            {
-              name = "ToggleMaximize";
-            }
-          ];
-        }
-        {
-          action = "Click";
-          button = "Right";
-          actions = [
-            {
-              name = "ToggleMaximize";
-              direction = "horizontal";
-            }
-          ];
-        }
-        {
-          action = "Click";
-          button = "Middle";
-          actions = [
-            {
-              name = "ToggleMaximize";
-              direction = "vertical";
-            }
-          ];
-        }
-      ];
-
-      "WindowMenu" = [
-        {
-          action = "Click";
-          button = "Left";
-          actions = [
-            {
-              name = "ShowMenu";
-              menu = "client-menu";
-              atCursor = "no";
-            }
-          ];
-        }
-        {
-          action = "Click";
-          button = "Right";
-          actions = [
-            {
-              name = "ShowMenu";
-              menu = "client-menu";
-              atCursor = "no";
-            }
-          ];
-        }
-      ];
-
-      "Icon" = [
-        {
-          action = "Click";
-          button = "Left";
-          actions = [
-            {
-              name = "ShowMenu";
-              menu = "client-menu";
-              atCursor = "no";
-            }
-          ];
-        }
-        {
-          action = "Click";
-          button = "Right";
-          actions = [
-            {
-              name = "ShowMenu";
-              menu = "client-menu";
-              atCursor = "no";
-            }
-          ];
-        }
-      ];
-
-      "Shade" = [
-        {
-          action = "Click";
-          button = "Left";
-          actions = [
-            {
-              name = "ToggleShade";
-            }
-          ];
-        }
-      ];
-
-      "AllDesktops" = [
-        {
-          action = "Click";
-          button = "Left";
-          actions = [
-            {
-              name = "ToggleOmnipresent";
-            }
-          ];
-        }
-      ];
-
-      "Iconify" = [
-        {
-          action = "Click";
-          button = "Left";
-          actions = [
-            {
-              name = "Iconify";
-            }
-          ];
-        }
-      ];
-
-      "Close" = [
-        {
-          action = "Click";
-          button = "Left";
-          actions = [
-            {
-              name = "Close";
-            }
-          ];
-        }
-      ];
-
-      "Client" = [
-        {
-          action = "Press";
-          button = "Left";
-          actions = [
-            {
-              name = "Focus";
-            }
-            {
-              name = "Raise";
-            }
-          ];
-        }
-        {
-          action = "Press";
-          button = "Middle";
-          actions = [
-            {
-              name = "Focus";
-            }
-            {
-              name = "Raise";
-            }
-          ];
-        }
-        {
-          action = "Press";
-          button = "Right";
-          actions = [
-            {
-              name = "Focus";
-            }
-            {
-              name = "Raise";
             }
           ];
         }
