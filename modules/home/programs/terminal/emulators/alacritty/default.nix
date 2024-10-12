f2fd7068944d0b36eb32b19168a3c8015c888900
Paul Nicholson <brenix@gmail.com>
2024-10-11 21:23:51 -0700

    update flake

--- modules/home/programs/terminal/emulators/alacritty/default.nix
+++ modules/home/programs/terminal/emulators/alacritty/default.nix
@@ -54,7 +54,7 @@ in {
 
         font = {
           # offset.y = -1;
-          size = mkForce 14;
+          size = mkForce 12;
           bold = {
             family = config.stylix.fonts.monospace.name;
             style = "Regular";
