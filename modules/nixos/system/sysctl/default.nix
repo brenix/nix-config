97539e5c203aa6e6bc1d56c116a14a7bcca99295
Paul Nicholson <brenix@gmail.com>
2025-01-02 13:38:51 -0800

    adjust sysctls; update flake

--- modules/nixos/system/sysctl/default.nix
+++ modules/nixos/system/sysctl/default.nix
@@ -17,9 +17,9 @@ in {
     boot = {
       kernel = {
         sysctl = {
-          "fs.file-max" = 5242880;
-          "fs.inotify.max_user_instances" = 1024;
-          "fs.inotify.max_user_watches" = 524288;
+          # "fs.file-max" = 5242880;
+          "fs.inotify.max_user_instances" = 8192;
+          "fs.inotify.max_user_watches" = 1048576;
           "fs.xfs.xfssyncd_centisecs" = 10000;
           "kernel.nmi_watchdog" = 0;
           "kernel.printk" = "3 3 3 3";
@@ -27,10 +27,10 @@ in {
           "kernel.unprivileged_userns_clone" = 1;
           "net.core.default_qdisc" = "fq";
           "net.core.netdev_max_backlog" = 16384;
-          "net.core.optmem_max" = 16777216;
-          "net.core.rmem_max" = 16777216;
+          "net.core.optmem_max" = 67108864;
+          "net.core.rmem_max" = 67108864;
           "net.core.somaxconn" = 8192;
-          "net.core.wmem_max" = 16777216;
+          "net.core.wmem_max" = 67108864;
           "net.ipv4.tcp_adv_win_scale" = -2;
           "net.ipv4.tcp_collapse_max_bytes" = 6291456;
           "net.ipv4.tcp_congestion_control" = "bbr";
@@ -43,7 +43,7 @@ in {
           "net.ipv4.tcp_mtu_probing" = 1;
           "net.ipv4.tcp_notsent_lowat" = 131072;
           "net.ipv4.tcp_retries1" = 2;
-          "net.ipv4.tcp_rmem" = "4096 32768 33554432";
+          "net.ipv4.tcp_rmem" = "4096 87380 33554432";
           "net.ipv4.tcp_sack" = 1;
           "net.ipv4.tcp_shrink_window" = 1;
           "net.ipv4.tcp_slow_start_after_idle" = 0;
@@ -52,7 +52,7 @@ in {
           "net.ipv4.tcp_syncookies" = 1;
           "net.ipv4.tcp_timestamps" = 1;
           "net.ipv4.tcp_tw_reuse" = 0;
-          "net.ipv4.tcp_wmem" = "4096 16384 33554432";
+          "net.ipv4.tcp_wmem" = "4096 65536 33554432";
           "net.ipv4.udp_rmem_min" = 8192;
           "net.ipv4.udp_wmem_min" = 8192;
           "vm.dirty_background_bytes" = 134217728;
