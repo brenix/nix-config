16a9c2799a56edce9b7fb4882911d44314edef47
Paul Nicholson <brenix@gmail.com>
2025-01-19 21:30:33 -0800

    tweaks

--- modules/home/programs/terminal/emulators/alacritty/default.nix
+++ modules/home/programs/terminal/emulators/alacritty/default.nix
@@ -55,8 +55,8 @@ in {
         };
 
         font = {
-          offset.y = -1;
-          size = mkForce 14;
+          offset.y = -2;
+          size = mkForce 12;
           bold = {
             family = config.stylix.fonts.monospace.name;
             style = "Regular";
