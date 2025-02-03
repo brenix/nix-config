bf3ff97f6b93c006de263e629ff729c02379e90a
Paul Nicholson <brenix@gmail.com>
2025-02-02 17:41:05 -0800

    bump font size

--- modules/home/programs/terminal/emulators/alacritty/default.nix
+++ modules/home/programs/terminal/emulators/alacritty/default.nix
@@ -56,7 +56,7 @@ in {
 
         font = {
           offset.y = -2;
-          size = mkForce 13;
+          size = mkForce 14;
           bold = {
             family = config.stylix.fonts.monospace.name;
             style = "Regular";
